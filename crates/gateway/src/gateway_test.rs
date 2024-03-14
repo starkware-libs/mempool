eed3f44c8dbecfa891adf06d880e0951aecf055c
Ayelet Zilber <ayelet@starkware.co>
2024-03-14 11:13:03 +0200

    Implement API Gateway skeleton and unit tests

---
+++ crates/gateway/src/gateway_test.rs
@@ -0,0 +1,18 @@
+use crate::gateway::handle_request;
+use crate::gateway::HandleContext;
+use hyper::{Body, Request};
+
+#[tokio::test]
+async fn test_invalid_request() {
+    // Create a sample GET request for an invalid path
+    let request = Request::get("/some_invalid_path")
+        .body(Body::empty())
+        .unwrap();
+    let response = handle_request(HandleContext {}, request).await.unwrap();
+
+    assert_eq!(response.status(), 404);
+    assert_eq!(
+        String::from_utf8_lossy(&hyper::body::to_bytes(response.into_body()).await.unwrap()),
+        "Not found."
+    );
+}
