dcfa712cd5dbb87365b708f2d0a7342513657942
Ayelet Zilber <ayelet@starkware.co>
2024-03-17 13:55:46 +0200

    Implement add_transaction in Gateway

---
+++ crates/gateway/src/test_utils.rs
@@ -0,0 +1,20 @@
+use starknet_api::core::ClassHash;
+use starknet_api::core::ContractAddress;
+use starknet_api::core::Nonce;
+use starknet_api::hash::StarkFelt;
+use starknet_api::transaction::DeclareTransaction;
+use starknet_api::transaction::{DeclareTransactionV0V1, Fee, Transaction, TransactionSignature};
+
+pub fn create_a_declare_tx() -> DeclareTransactionV0V1 {
+    DeclareTransactionV0V1 {
+        max_fee: Fee(0),
+        signature: TransactionSignature::default(),
+        nonce: Nonce(StarkFelt::from(1_u8)),
+        class_hash: ClassHash(StarkFelt::from(1_u8)),
+        sender_address: ContractAddress::try_from(StarkFelt::from(1_u8)).unwrap(),
+    }
+}
+
+pub fn create_transaction(declare_type: DeclareTransaction) -> Transaction {
+    Transaction::Declare(declare_type)
+}
