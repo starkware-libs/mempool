f7972ff81bef57f9112048f32bf664fc0008f271
Arni Hod <arnon@starkware.co>
2024-03-17 09:49:34 +0200

    feat(gateway): add fee non zero validation

--- crates/gateway/src/errors.rs
+++ crates/gateway/src/errors.rs
@@ -4,13 +4,24 @@ use thiserror::Error;
 
 #[derive(Debug, Error)]
 #[cfg_attr(test, derive(PartialEq))]
+pub enum StarknetApiTransactionError {
+    #[error("This transaction type is not supported by the mempool")]
+    TransactionTypeNotSupported,
+}
+
+pub type StarknetApiTransactionResult<T> = Result<T, StarknetApiTransactionError>;
+
+#[derive(Debug, Error)]
+#[cfg_attr(test, derive(PartialEq))]
 pub enum TransactionValidatorError {
+    #[error(transparent)]
+    StarknetApiTransactionError(#[from] StarknetApiTransactionError),
     #[error("Transactions of version {0:?} are not valid. {1}")]
     InvalidTransactionVersion(TransactionVersion, String),
     #[error("Blocked transaction version {0:?}. {1}")]
     BlockedTransactionVersion(TransactionVersion, String),
-    #[error("This transaction type is not supported by the mempool")]
-    TransactionTypeNotSupported,
+    #[error("Transaction must commit to pay a positive amount on fee.")]
+    ZeroFee,
 }
 
 pub type TransactionValidatorResult<T> = Result<T, TransactionValidatorError>;
