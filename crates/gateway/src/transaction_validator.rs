ebac973e5b02208bb1a02a58fd62e1dbaf0fe3e9
Arni Hod <arnon@starkware.co>
2024-03-17 09:49:34 +0200

    feat: create skelaton of transaction validator

---
+++ crates/gateway/src/transaction_validator.rs
@@ -0,0 +1,73 @@
+use starknet_api::transaction::{Transaction, TransactionVersion};
+
+use crate::errors::{
+    StarknetApiTransactionError, TransactionValidatorError, TransactionValidatorResult,
+};
+use crate::starknet_api_utils::TransactionVersionExt;
+
+#[cfg(test)]
+#[path = "transaction_validator_test.rs"]
+mod transaction_validator_test;
+
+#[derive(Default)]
+pub struct TransactionValidatorConfig {
+    pub block_declare_cairo1: bool,
+    pub block_declare_cairo0: bool,
+
+    pub min_allowed_tx_version: TransactionVersion,
+    pub max_allowed_tx_version: TransactionVersion,
+}
+
+pub struct TransactionValidator {
+    pub config: TransactionValidatorConfig,
+}
+
+impl TransactionValidator {
+    pub fn validate(&self, tx: Transaction) -> TransactionValidatorResult<()> {
+        // Deploy transactions are deprecated.
+        // L1Handler transactions are not supported in the gateway.
+        if matches!(tx, Transaction::Deploy(_) | Transaction::L1Handler(_)) {
+            return Err(StarknetApiTransactionError::TransactionTypeNotSupported.into());
+        }
+
+        // Check if the declaration of Cairo / Cairo-0 contracts are blocked.
+        if let Transaction::Declare(tx) = &tx {
+            if tx.version() < TransactionVersion::TWO && self.config.block_declare_cairo0 {
+                return Err(TransactionValidatorError::BlockedTransactionVersion(
+                    tx.version(),
+                    "Declare of Cairo 0 is blocked.".into(),
+                ));
+            }
+            if tx.version() >= TransactionVersion::TWO && self.config.block_declare_cairo1 {
+                return Err(TransactionValidatorError::BlockedTransactionVersion(
+                    tx.version(),
+                    "Transaction type is temporarily blocked.".into(),
+                ));
+            }
+        }
+
+        // TODO(Arni, 1/4/2024): Add a mechanism that validate the sender address is not blocked.
+        let version = tx.version();
+        if version < self.config.min_allowed_tx_version {
+            return Err(TransactionValidatorError::InvalidTransactionVersion(
+                version,
+                format!(
+                    "Minimal supported version is {:?}.",
+                    self.config.min_allowed_tx_version
+                ),
+            ));
+        }
+        if version > self.config.max_allowed_tx_version {
+            return Err(TransactionValidatorError::InvalidTransactionVersion(
+                version,
+                format!(
+                    "Maximal supported version is {:?}.",
+                    self.config.max_allowed_tx_version
+                ),
+            ));
+        }
+
+        // TODO(Arni, 1/4/2024): Validate fee and tx size.
+        Ok(())
+    }
+}
