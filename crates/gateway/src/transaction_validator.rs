952c4dd200b27526495c620940fdb40dd566f204
Arni Hod <arnon@starkware.co>
2024-03-20 15:02:11 +0200

    feat(gateway): add fee non zero validation

--- crates/gateway/src/transaction_validator.rs
+++ crates/gateway/src/transaction_validator.rs
@@ -1,4 +1,7 @@
-use starknet_api::transaction::{Transaction, TransactionVersion};
+use starknet_api::transaction::{
+    DeclareTransaction, DeployAccountTransaction, Fee, InvokeTransaction, ResourceBoundsMapping,
+    Transaction, TransactionVersion,
+};
 
 use thiserror::Error;
 
@@ -9,12 +12,14 @@ mod transaction_validator_test;
 #[derive(Debug, Error)]
 #[cfg_attr(test, derive(PartialEq))]
 pub enum TransactionValidatorError {
-    #[error("Invalid transaction type")]
-    InvalidTransactionType,
+    #[error("This transaction type is not supported by the mempool")]
+    TransactionTypeNotSupported,
     #[error("Transactions of version {0:?} are not valid. {1}")]
     InvalidTransactionVersion(TransactionVersion, String),
     #[error("Blocked transaction version {0:?}. {1}")]
     BlockedTransactionVersion(TransactionVersion, String),
+    #[error("Transaction must commit to pay a positive amount on fee.")]
+    ZeroFee,
 }
 
 pub type TransactionValidatorResult<T> = Result<T, TransactionValidatorError>;
@@ -26,6 +31,8 @@ pub struct TransactionValidatorConfig {
 
     pub min_allowed_tx_version: TransactionVersion,
     pub max_allowed_tx_version: TransactionVersion,
+
+    pub enforce_fee: bool,
 }
 
 pub struct TransactionValidator {
@@ -37,7 +44,7 @@ impl TransactionValidator {
         // Deploy transactions are deprecated.
         // L1Handler transactions are not supported in the gateway.
         if matches!(tx, Transaction::Deploy(_) | Transaction::L1Handler(_)) {
-            return Err(TransactionValidatorError::InvalidTransactionType);
+            return Err(TransactionValidatorError::TransactionTypeNotSupported);
         }
 
         // Check if the declaration of Cairo / Cairo-0 contracts are blocked.
@@ -77,8 +84,100 @@ impl TransactionValidator {
             ));
         }
 
-        // TODO(Arni, 1/4/2024): Validate fee and tx size.
+        // TODO(Arni, 1/4/2024): Validate tx size.
+        self.validate_fee(&tx)?;
+
         Ok(())
+    }
+
+    fn validate_fee(&self, tx: &Transaction) -> TransactionValidatorResult<()> {
+        if !self.config.enforce_fee {
+            return Ok(());
+        }
+
+        if tx.max_fee()? == Fee(0) {
+            return Err(TransactionValidatorError::ZeroFee);
+        }
+
+        Ok(())
+    }
+}
+
+trait MaxFeeExt {
+    fn max_fee(&self) -> TransactionValidatorResult<Fee>;
+}
+
+impl MaxFeeExt for Transaction {
+    fn max_fee(&self) -> TransactionValidatorResult<Fee> {
+        match self {
+            Transaction::Declare(declare_tx) => declare_tx.max_fee(),
+            Transaction::DeployAccount(deploy_account_tx) => deploy_account_tx.max_fee(),
+            Transaction::Invoke(invoke_tx) => invoke_tx.max_fee(),
+            Transaction::Deploy(_) => Err(TransactionValidatorError::TransactionTypeNotSupported),
+            Transaction::L1Handler(_) => {
+                Err(TransactionValidatorError::TransactionTypeNotSupported)
+            }
+        }
+    }
+}
+
+impl MaxFeeExt for DeclareTransaction {
+    fn max_fee(&self) -> TransactionValidatorResult<Fee> {
+        match self {
+            starknet_api::transaction::DeclareTransaction::V0(_)
+            | starknet_api::transaction::DeclareTransaction::V1(_)
+            | starknet_api::transaction::DeclareTransaction::V2(_) => {
+                Err(TransactionValidatorError::TransactionTypeNotSupported)
+                // This code will return the proper max_fee.
+                // _deprecated_declare_tx.max_fee
+            }
+            starknet_api::transaction::DeclareTransaction::V3(declare_v3_tx) => {
+                declare_v3_tx.resource_bounds.max_fee()
+            }
+        }
+    }
+}
+
+impl MaxFeeExt for DeployAccountTransaction {
+    fn max_fee(&self) -> TransactionValidatorResult<Fee> {
+        match self {
+            starknet_api::transaction::DeployAccountTransaction::V1(_) => {
+                Err(TransactionValidatorError::TransactionTypeNotSupported)
+                // This code will return the proper max_fee.
+                // _deprecated_deploy_account_tx.max_fee
+            }
+            starknet_api::transaction::DeployAccountTransaction::V3(deploy_account_v3_tx) => {
+                deploy_account_v3_tx.resource_bounds.max_fee()
+            }
+        }
+    }
+}
+
+impl MaxFeeExt for InvokeTransaction {
+    fn max_fee(&self) -> TransactionValidatorResult<Fee> {
+        match self {
+            starknet_api::transaction::InvokeTransaction::V0(_)
+            | starknet_api::transaction::InvokeTransaction::V1(_) => {
+                Err(TransactionValidatorError::TransactionTypeNotSupported)
+                // This code will return the proper max_fee.
+                // _deprecated_invoke_tx.max_fee
+            }
+            starknet_api::transaction::InvokeTransaction::V3(invoke_v3_tx) => {
+                invoke_v3_tx.resource_bounds.max_fee()
+            }
+        }
+    }
+}
+
+impl MaxFeeExt for ResourceBoundsMapping {
+    fn max_fee(&self) -> TransactionValidatorResult<Fee> {
+        let l1_bounds = self
+            .0
+            .get(&starknet_api::transaction::Resource::L1Gas)
+            .expect("resource bounds should contain L1Gas.");
+        let max_amount = l1_bounds.max_amount;
+        let max_price_per_unit = l1_bounds.max_price_per_unit;
+        Ok(Fee(u128::from(max_amount) * max_price_per_unit))
     }
 }
 
--- crates/gateway/src/transaction_validator_test.rs
+++ crates/gateway/src/transaction_validator_test.rs
@@ -1,7 +1,11 @@
 use rstest::rstest;
 
-use starknet_api::transaction::{
-    InvokeTransaction, InvokeTransactionV1, Transaction, TransactionVersion,
+use starknet_api::{
+    calldata,
+    transaction::{
+        Calldata, InvokeTransaction, InvokeTransactionV1, InvokeTransactionV3, ResourceBounds,
+        ResourceBoundsMapping, Transaction, TransactionVersion,
+    },
 };
 
 use crate::transaction_validator::{
@@ -64,22 +68,77 @@ use crate::transaction_validator::{
         ..Default::default()
     },
     Transaction::Deploy(starknet_api::transaction::DeployTransaction {..Default::default()}),
-    Some(TransactionValidatorError::InvalidTransactionType)
+    Some(TransactionValidatorError::TransactionTypeNotSupported)
 )]
 #[case::unsupported_l1_handler_tx(
     TransactionValidatorConfig {
         ..Default::default()
     },
     Transaction::L1Handler(starknet_api::transaction::L1HandlerTransaction {..Default::default()}),
-    Some(TransactionValidatorError::InvalidTransactionType)
+    Some(TransactionValidatorError::TransactionTypeNotSupported)
+)]
+#[case::invalid_max_fee(
+    TransactionValidatorConfig {
+        min_allowed_tx_version: TransactionVersion::ZERO,
+        max_allowed_tx_version: TransactionVersion::THREE,
+        enforce_fee: true,
+        ..Default::default()
+    },
+    Transaction::Invoke(InvokeTransaction::V3(InvokeTransactionV3 {
+        resource_bounds:ResourceBoundsMapping::try_from(
+            vec![
+                (
+                    starknet_api::transaction::Resource::L1Gas,
+                    ResourceBounds{max_amount: 1, max_price_per_unit: 0 }
+                ),
+                (
+                    starknet_api::transaction::Resource::L2Gas,
+                    ResourceBounds{max_amount: 0, max_price_per_unit: 0 }
+                )
+            ]
+        ).unwrap(),
+        tip: Default::default(),
+        signature: Default::default(),
+        nonce: Default::default(),
+        sender_address: Default::default(),
+        calldata: calldata!(),
+        nonce_data_availability_mode: starknet_api::data_availability::DataAvailabilityMode::L1,
+        fee_data_availability_mode: starknet_api::data_availability::DataAvailabilityMode::L1,
+        paymaster_data: Default::default(),
+        account_deployment_data: Default::default()
+    })),
+    Some(TransactionValidatorError::ZeroFee)
 )]
 #[case::valid_tx(
     TransactionValidatorConfig {
         min_allowed_tx_version: TransactionVersion::ZERO,
         max_allowed_tx_version: TransactionVersion::THREE,
+        enforce_fee: true,
         ..Default::default()
     },
-    Transaction::Invoke(InvokeTransaction::V1(InvokeTransactionV1 {..Default::default()})),
+    Transaction::Invoke(InvokeTransaction::V3(InvokeTransactionV3 {
+        resource_bounds:ResourceBoundsMapping::try_from(
+            vec![
+                (
+                    starknet_api::transaction::Resource::L1Gas,
+                    ResourceBounds{max_amount: 1, max_price_per_unit: 1 }
+                ),
+                (
+                    starknet_api::transaction::Resource::L2Gas,
+                    ResourceBounds{max_amount: 0, max_price_per_unit: 0 }
+                )
+            ]
+        ).unwrap(),
+        tip: Default::default(),
+        signature: Default::default(),
+        nonce: Default::default(),
+        sender_address: Default::default(),
+        calldata: calldata!(),
+        nonce_data_availability_mode: starknet_api::data_availability::DataAvailabilityMode::L1,
+        fee_data_availability_mode: starknet_api::data_availability::DataAvailabilityMode::L1,
+        paymaster_data: Default::default(),
+        account_deployment_data: Default::default()
+    })),
     None
 )]
 fn test_transaction_version(
