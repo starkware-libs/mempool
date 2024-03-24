ad17387d4608faddbeae3f16937044af6b999f0e
Mohammad Nassar <mohammad@starkware.co>
2024-03-24 11:16:19 +0200

    feat(gateway): Add transaction hash calculation

--- crates/gateway/src/gateway.rs
+++ crates/gateway/src/gateway.rs
@@ -1,7 +1,10 @@
+use tokio::task;
+
 use crate::errors::{GatewayConfigError, GatewayError};
 use axum::response::IntoResponse;
 use axum::routing::{get, post};
 use axum::{Json, Router};
+use starknet_api::core::ChainId;
 use starknet_api::external_transaction::ExternalTransaction;
 use std::net::SocketAddr;
 use std::str::FromStr;
@@ -12,21 +15,26 @@ pub mod gateway_test;
 
 pub type GatewayResult = Result<(), GatewayError>;
 
+#[derive(Clone)]
 pub struct Gateway {
-    pub gateway_config: GatewayConfig,
+    pub config: GatewayConfig,
 }
 
 impl Gateway {
-    pub async fn build_server(&self) -> GatewayResult {
+    pub async fn build_server(self) -> GatewayResult {
         // Parses the bind address from GatewayConfig, returning an error for invalid addresses.
-        let addr = SocketAddr::from_str(&self.gateway_config.bind_address).map_err(|_| {
-            GatewayConfigError::InvalidServerBindAddress(self.gateway_config.bind_address.clone())
+        let addr = SocketAddr::from_str(&self.config.bind_address).map_err(|_| {
+            GatewayConfigError::InvalidServerBindAddress(self.config.bind_address.clone())
         })?;
+        let gateway = self.clone();
 
         // Sets up the router with the specified routes for the server.
-        let app = Router::new()
-            .route("/is_alive", get(is_alive))
-            .route("/add_transaction", post(add_transaction));
+        let app = Router::new().route("/is_alive", get(is_alive)).route(
+            "/add_transaction",
+            post(move |request_body: Json<ExternalTransaction>| {
+                Self::add_transaction(gateway, request_body)
+            }),
+        );
 
         // Create a server that runs forever.
         axum::Server::bind(&addr)
@@ -36,20 +44,34 @@ impl Gateway {
 
         Ok(())
     }
+
+    async fn add_transaction(
+        self,
+        Json(transaction_json): Json<ExternalTransaction>,
+    ) -> impl IntoResponse {
+        let tx_type = match transaction_json {
+            ExternalTransaction::Declare(_) => "DECLARE",
+            ExternalTransaction::DeployAccount(_) => "DEPLOY_ACCOUNT",
+            ExternalTransaction::Invoke(_) => "INVOKE",
+        };
+
+        let transaction_clone = transaction_json.clone();
+        let config_clone = self.config.clone();
+        task::spawn_blocking(move || {
+            // Simulate a heavy computation
+            let _internal_tx = transaction_clone.into_internal(&config_clone.chain_id);
+        });
+
+        tx_type
+    }
 }
 
+#[derive(Clone)]
 pub struct GatewayConfig {
     pub bind_address: String,
+    pub chain_id: ChainId,
 }
 
 async fn is_alive() -> impl IntoResponse {
     unimplemented!("Future handling should be implemented here.");
-}
-
-async fn add_transaction(Json(transaction_json): Json<ExternalTransaction>) -> impl IntoResponse {
-    match transaction_json {
-        ExternalTransaction::Declare(_) => "DECLARE",
-        ExternalTransaction::DeployAccount(_) => "DEPLOY_ACCOUNT",
-        ExternalTransaction::Invoke(_) => "INVOKE",
-    }
 }
