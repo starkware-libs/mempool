aba753a9677f1c4e7fabaeb44f030422f23314e0
Mohammad Nassar <mohammad@starkware.co>
2024-03-24 11:16:19 +0200

    feat(gateway): Add transaction hash calculation

--- crates/gateway/src/gateway_test.rs
+++ crates/gateway/src/gateway_test.rs
@@ -1,4 +1,3 @@
-use crate::gateway::add_transaction;
 use axum::{
     body::{Bytes, HttpBody},
     response::{IntoResponse, Response},
@@ -7,6 +6,8 @@ use pretty_assertions::assert_str_eq;
 use rstest::rstest;
 use starknet_api::external_transaction::ExternalTransaction;
 use std::fs::File;
+
+use crate::gateway;
 
 // TODO(Ayelet): Replace the use of the JSON files with generated instances, then serialize these
 // into JSON for testing.
@@ -19,10 +20,19 @@ use std::fs::File;
 #[case("./src/json_files_for_testing/invoke_v3.json", "INVOKE")]
 #[tokio::test]
 async fn test_add_transaction(#[case] json_file_path: &str, #[case] expected_response: &str) {
+    use starknet_api::core::ChainId;
+
     let json_file = File::open(json_file_path).unwrap();
     let tx: ExternalTransaction = serde_json::from_reader(json_file).unwrap();
 
-    let response = add_transaction(tx.into()).await.into_response();
+    let gateway = gateway::Gateway {
+        config: gateway::GatewayConfig {
+            bind_address: "0.0.0.0:8080".to_string(),
+            chain_id: ChainId("SN_MAIN".to_string()),
+        },
+    };
+
+    let response = gateway.add_transaction(tx.into()).await.into_response();
     let response_bytes = &to_bytes(response).await;
 
     assert_str_eq!(&String::from_utf8_lossy(response_bytes), expected_response);
