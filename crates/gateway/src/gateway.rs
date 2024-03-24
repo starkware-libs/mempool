b16a0753390e83087866fa5f18c96a4ed5d7d55e
Mohammad Nassar <mohammad@starkware.co>
2024-03-24 11:16:19 +0200

    feat(gateway): add transaction hash calculation

--- crates/gateway/src/gateway.rs
+++ crates/gateway/src/gateway.rs
@@ -1,3 +1,8 @@
+use serde::Deserialize;
+use starknet_api::core::ChainId;
+use starknet_api::internal_transaction::InternalTransaction;
+use tokio::task;
+
 use crate::errors::GatewayError;
 use axum::response::IntoResponse;
 use axum::routing::{get, post};
@@ -11,8 +16,10 @@ pub mod gateway_test;
 
 pub type GatewayResult = Result<(), GatewayError>;
 
+#[derive(Clone)]
 pub struct Gateway {
     pub config: GatewayConfig,
+    pub chain_id: ChainId,
 }
 
 impl Gateway {
@@ -44,14 +51,23 @@ async fn is_alive() -> impl IntoResponse {
     unimplemented!("Future handling should be implemented here.");
 }
 
-async fn add_transaction(Json(transaction): Json<ExternalTransaction>) -> impl IntoResponse {
-    match transaction {
-        ExternalTransaction::Declare(_) => "DECLARE",
-        ExternalTransaction::DeployAccount(_) => "DEPLOY_ACCOUNT",
-        ExternalTransaction::Invoke(_) => "INVOKE",
-    }
+async fn add_transaction(Json(input): Json<GatewayTransactionInput>) -> impl IntoResponse {
+    let transaction_clone = input.tx.clone();
+    let handle = task::spawn_blocking(move || {
+        // Simulate a heavy computation
+        transaction_clone.into_internal(&input.chain_id)
+    });
+
+    let internal_tx = handle.await.unwrap();
+    let internal_tx = match internal_tx {
+        InternalTransaction::Declare(tx) => tx.tx_hash,
+        InternalTransaction::DeployAccount(tx) => tx.tx_hash,
+        InternalTransaction::Invoke(tx) => tx.tx_hash,
+    };
+    internal_tx.to_string().into_response()
 }
 
+#[derive(Clone, Debug)]
 pub struct GatewayConfig {
     pub ip: IpAddr,
     pub port: u16,
@@ -61,4 +77,10 @@ impl GatewayConfig {
     pub fn new(ip: IpAddr, port: u16) -> Self {
         Self { ip, port }
     }
+}
+
+#[derive(Deserialize)]
+pub struct GatewayTransactionInput {
+    chain_id: ChainId,
+    tx: ExternalTransaction,
 }
--- crates/gateway/src/gateway_test.rs
+++ crates/gateway/src/gateway_test.rs
@@ -1,10 +1,12 @@
 use crate::gateway::add_transaction;
+use crate::gateway::GatewayTransactionInput;
 use axum::{
     body::{Bytes, HttpBody},
     response::{IntoResponse, Response},
 };
 use pretty_assertions::assert_str_eq;
 use rstest::rstest;
+use starknet_api::core::ChainId;
 use starknet_api::external_transaction::ExternalTransaction;
 use std::fs::File;
 use std::path::Path;
@@ -14,18 +16,20 @@ const TEST_FILES_FOLDER: &str = "./tests/fixtures";
 // TODO(Ayelet): Replace the use of the JSON files with generated instances, then serialize these
 // into JSON for testing.
 #[rstest]
-#[case::declare(&Path::new(TEST_FILES_FOLDER).join("declare_v3.json"), "DECLARE")]
+#[case::declare(&Path::new(TEST_FILES_FOLDER).join("declare_v3.json"), "0x03822dbc50d129064b16e3ed3ff1af2cb34cdb15f202ea6c5ec98f1cc0190ede")]
 #[case::deploy_account(
     &Path::new(TEST_FILES_FOLDER).join("deploy_account_v3.json"),
-    "DEPLOY_ACCOUNT"
+    "0x0274837d32404e10c2aba2c782854abd692eaed9e4d46676f5611c90de6979f9"
 )]
-#[case::invoke(&Path::new(TEST_FILES_FOLDER).join("invoke_v3.json"), "INVOKE")]
+#[case::invoke(&Path::new(TEST_FILES_FOLDER).join("invoke_v3.json"), "0x06401db149315bdd1370b04c911d2e83789017574665744afa257bc1abb00308")]
 #[tokio::test]
 async fn test_add_transaction(#[case] json_file_path: &Path, #[case] expected_response: &str) {
     let json_file = File::open(json_file_path).unwrap();
+    let chain_id = ChainId("SN_TEST".to_string());
     let tx: ExternalTransaction = serde_json::from_reader(json_file).unwrap();
+    let input = GatewayTransactionInput { chain_id, tx };
 
-    let response = add_transaction(tx.into()).await.into_response();
+    let response = add_transaction(input.into()).await.into_response();
     let response_bytes = &to_bytes(response).await;
 
     assert_str_eq!(&String::from_utf8_lossy(response_bytes), expected_response);
