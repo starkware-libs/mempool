41a70a0d1c1957e7c6eade1ce7b2b37f8a3e492e
Ayelet Zilber <ayelet@starkware.co>
2024-03-27 17:07:49 +0200

    feat(gateway): Implement add_transaction

--- crates/gateway/src/gateway_test.rs
+++ crates/gateway/src/gateway_test.rs
@@ -1,3 +1,4 @@
+use crate::gateway::add_transaction;
 use crate::gateway::handle_request;
 use hyper::{Body, Request};
 
@@ -14,4 +15,27 @@ async fn test_invalid_request() {
         String::from_utf8_lossy(&hyper::body::to_bytes(response.into_body()).await.unwrap()),
         "Not found."
     );
+}
+
+// TODO(Ayelet): Replace the use of the JSON files with generated instances, then serialize these
+// into JSON for testing.
+#[rstest::rstest]
+#[case("./src/json_files_for_testing/declare_v3.json", "DECLARE")]
+#[case(
+    "./src/json_files_for_testing/deploy_account_v3.json",
+    "DEPLOY_ACCOUNT"
+)]
+#[case("./src/json_files_for_testing/invoke_v3.json", "INVOKE")]
+#[tokio::test]
+async fn test_add_transaction(#[case] json_file_path: &str, #[case] expected_response: &str) {
+    let json_str = std::fs::read_to_string(json_file_path).expect("Failed to read JSON file");
+    let body = Body::from(json_str);
+    let response = add_transaction(body)
+        .await
+        .expect("Failed to process transaction");
+    let bytes = hyper::body::to_bytes(response.into_body())
+        .await
+        .expect("Failed to read response body");
+    let body_str = String::from_utf8(bytes.to_vec()).expect("Response body is not valid UTF-8");
+    assert_eq!(body_str, expected_response);
 }
