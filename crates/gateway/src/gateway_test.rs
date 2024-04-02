77417a6e463cc38a4f5a00a3d3a596fe033f55a9
Ayelet Zilber <ayelet@starkware.co>
2024-04-02 16:48:03 +0300

    refactor(gateway): use axum instead of hyper

--- crates/gateway/src/gateway_test.rs
+++ crates/gateway/src/gateway_test.rs
@@ -1,23 +1,8 @@
 use crate::gateway::add_transaction;
-use crate::gateway::handle_request;
-use hyper::{body, Body, Request};
+use axum::{body::HttpBody, response::IntoResponse};
 use rstest::rstest;
+use starknet_api::external_transaction::ExternalTransaction;
 use std::fs;
-
-#[tokio::test]
-async fn test_invalid_request() {
-    // Create a sample GET request for an invalid path
-    let request = Request::get("/some_invalid_path")
-        .body(Body::empty())
-        .unwrap();
-    let response = handle_request(request).await.unwrap();
-
-    assert_eq!(response.status(), 404);
-    assert_eq!(
-        String::from_utf8_lossy(&body::to_bytes(response.into_body()).await.unwrap()),
-        "Not found."
-    );
-}
 
 // TODO(Ayelet): Replace the use of the JSON files with generated instances, then serialize these
 // into JSON for testing.
@@ -31,13 +16,12 @@ async fn test_invalid_request() {
 #[tokio::test]
 async fn test_add_transaction(#[case] json_file_path: &str, #[case] expected_response: &str) {
     let json_str = fs::read_to_string(json_file_path).expect("Failed to read JSON file");
-    let body = Body::from(json_str);
-    let response = add_transaction(body)
-        .await
-        .expect("Failed to process transaction");
-    let bytes = body::to_bytes(response.into_body())
-        .await
-        .expect("Failed to read response body");
-    let body_str = String::from_utf8(bytes.to_vec()).expect("Response body is not valid UTF-8");
-    assert_eq!(body_str, expected_response);
+    let transaction: ExternalTransaction =
+        serde_json::from_str(&json_str).expect("Failed to parse JSON");
+    let response = add_transaction(transaction.into()).await.into_response();
+    let response_bytes = response.into_body().collect().await.unwrap().to_bytes();
+    assert_eq!(
+        &String::from_utf8(response_bytes.to_vec()).unwrap(),
+        expected_response
+    );
 }
