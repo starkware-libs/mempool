655c5c8b01d6abf4ca99a0bd88aafdb81731e0bd
Lev Roitman <lev@starkware.co>
2024-04-03 18:27:48 +0300

    feat: add config with papyrus_config

--- crates/gateway/src/gateway_test.rs
+++ crates/gateway/src/gateway_test.rs
@@ -1,9 +1,14 @@
 use crate::gateway::add_transaction;
+use crate::GatewayConfig;
 use axum::{body::HttpBody, response::IntoResponse};
+use clap::Command;
+use papyrus_config::loading::load_and_process_config;
 use rstest::rstest;
 use starknet_api::external_transaction::ExternalTransaction;
 use std::fs::File;
 use std::io::BufReader;
+use std::path::Path;
+use validator::Validate;
 
 // TODO(Ayelet): Replace the use of the JSON files with generated instances, then serialize these
 // into JSON for testing.
@@ -25,4 +30,48 @@ async fn test_add_transaction(#[case] json_file_path: &str, #[case] expected_res
         &String::from_utf8(response_bytes.to_vec()).unwrap(),
         expected_response
     );
+}
+
+const DEFAULT_GOOD_CONFIG_PATH: &str = "./src/json_files_for_testing/good_gateway_config.json";
+const DEFAULT_BAD_CONFIG_PATH: &str = "./src/json_files_for_testing/bad_gateway_config.json";
+const DEFAULT_BAD_ADDRESS_CONFIG_PATH: &str =
+    "./src/json_files_for_testing/bad_gateway_address_config.json";
+
+#[test]
+fn good_config_test() {
+    // Read the good config file and validate it.
+    let config = GatewayConfig::default();
+
+    let config_file = File::open(Path::new(DEFAULT_GOOD_CONFIG_PATH)).unwrap();
+    let load_config =
+        load_and_process_config::<GatewayConfig>(config_file, Command::new(""), vec![]).unwrap();
+    assert!(load_config.validate().is_ok());
+    assert_eq!(load_config.bind_address, config.bind_address);
+}
+
+#[test]
+fn bad_config_test() {
+    // Read the config file with the bad field path and validate it.
+    let config_file = std::fs::File::open(Path::new(DEFAULT_BAD_CONFIG_PATH)).unwrap();
+    let load_config =
+        load_and_process_config::<GatewayConfig>(config_file, Command::new(""), vec![]);
+    match load_config {
+        Ok(_) => panic!("Expected an error, but got a config."),
+        Err(e) => assert_eq!(e.to_string(), "missing field `bind_address`".to_owned()),
+    }
+}
+
+#[test]
+fn bad_config_address_test() {
+    // Read the config file with the bad bind_address values and validate it
+    let config_file = std::fs::File::open(Path::new(DEFAULT_BAD_ADDRESS_CONFIG_PATH)).unwrap();
+    let load_config =
+        load_and_process_config::<GatewayConfig>(config_file, Command::new(""), vec![]).unwrap();
+    match load_config.validate() {
+        Ok(_) => panic!("Expected an error, but got a config."),
+        Err(e) => assert_eq!(
+            e.field_errors()["bind_address"][0].code,
+            "Invalid Socket address.".to_owned()
+        ),
+    }
 }
--- crates/gateway/src/lib.rs
+++ crates/gateway/src/lib.rs
@@ -1,2 +1,47 @@
 pub mod errors;
 pub mod gateway;
+
+use serde::{Deserialize, Serialize};
+use std::collections::BTreeMap;
+use std::str::FromStr;
+
+use papyrus_config::dumping::{ser_param, SerializeConfig};
+use papyrus_config::{ParamPath, ParamPrivacyInput, SerializedParam};
+use std::net::SocketAddr;
+use validator::{Validate, ValidationError};
+
+/// Configuration for a gateway.
+#[derive(Clone, Debug, Serialize, Deserialize, Validate, PartialEq)]
+pub struct GatewayConfig {
+    /// The gateway bind address.
+    #[validate(custom = "validate_ip_address")]
+    pub bind_address: String,
+}
+
+impl SerializeConfig for GatewayConfig {
+    fn dump(&self) -> BTreeMap<ParamPath, SerializedParam> {
+        BTreeMap::from_iter([ser_param(
+            "gateway.server_bind_address",
+            &self.bind_address,
+            "The server bind addres of a gateway.",
+            ParamPrivacyInput::Public,
+        )])
+    }
+}
+
+impl Default for GatewayConfig {
+    fn default() -> Self {
+        Self {
+            bind_address: String::from("0.0.0.0:8080"),
+        }
+    }
+}
+
+pub fn validate_ip_address(ip_address: &str) -> Result<(), ValidationError> {
+    if SocketAddr::from_str(ip_address).is_err() {
+        let mut error = ValidationError::new("Invalid Socket address.");
+        error.message = Some("Please provide valid Socket address in the configuration.".into());
+        return Err(error);
+    }
+    Ok(())
+}
