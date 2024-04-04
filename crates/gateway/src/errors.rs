9aa64aed3f1688b40b7d6e4b7c22e504e0fba0be
Gilad Chase <gilad@starkware.com>
2024-04-04 18:29:48 +0300

    refactor: simplify and reorganize `Gateway`
    - use more concise types for ip/port to make cast to IP (and config creation in general) error-free.
    - extract routes to dedicated function: this practice and naming as `app` a known idiom: this makes
    for easier testing later on, and also will contain other configurations later on like banned IPs.
    - Fix naming of arg in `add_transaction`: the arg is already
      deserialized from json, so no _json suffix.

--- crates/gateway/src/errors.rs
+++ crates/gateway/src/errors.rs
@@ -3,8 +3,6 @@ use thiserror::Error;
 #[derive(Debug, Error)]
 pub enum GatewayError {
     #[error(transparent)]
-    ConfigError(#[from] GatewayConfigError),
-    #[error(transparent)]
     HTTPError(#[from] hyper::http::Error),
     #[error("Internal server error")]
     InternalServerError,
@@ -12,10 +10,4 @@ pub enum GatewayError {
     InvalidTransactionFormat(#[from] serde_json::Error),
     #[error("Error while starting the server")]
     ServerStartError(#[from] hyper::Error),
-}
-
-#[derive(Debug, Error)]
-pub enum GatewayConfigError {
-    #[error("Server address is not an bind IP address: {0}")]
-    InvalidServerBindAddress(String),
 }
