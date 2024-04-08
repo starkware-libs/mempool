64005f2d6b82e19226d419c4d573b85f716e02b4
Itay Tsabary <itayt@starkware.co>
2024-04-08 11:45:54 +0300

    Enhance clippy
    commit-id:e46388d3

--- scripts/clippy.sh
+++ scripts/clippy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cargo clippy "$@" --all-targets --all-features 
+cargo clippy "$@" --all-targets --all-features -- -D warnings -D future-incompatible -D nonstandard-style -D rust-2018-idioms -D unused -D clippy::unwrap_used -A clippy::blocks_in_conditions
