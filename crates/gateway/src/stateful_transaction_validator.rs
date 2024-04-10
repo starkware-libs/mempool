a58063fba0f391b3c3373632ce910c307a337874
Yael Doweck <yael@starkware.co>
2024-04-10 17:09:41 +0300

    feat: add stateful validations

---
+++ crates/gateway/src/stateful_transaction_validator.rs
@@ -0,0 +1,70 @@
+use blockifier::blockifier::block::BlockInfo;
+use blockifier::blockifier::stateful_validator::StatefulValidator as BlockifierStatefulValidator;
+use blockifier::bouncer::BouncerConfig;
+use blockifier::context::BlockContext;
+use blockifier::context::ChainInfo;
+use blockifier::execution::contract_class::ClassInfo;
+use blockifier::state::cached_state::CachedState;
+use blockifier::state::state_api::StateReader;
+
+use blockifier::versioned_constants::VersionedConstants;
+use starknet_api::core::Nonce;
+use starknet_api::external_transaction::ExternalTransaction;
+use starknet_api::transaction::TransactionHash;
+
+use crate::errors::StatefulTransactionValidatorError;
+use crate::errors::StatefulTransactionValidatorResult;
+use crate::utils::external_tx_to_account_tx;
+
+pub struct StatefulTransactionValidatorConfig {
+    pub max_nonce_for_validation_skip: Nonce,
+    pub validate_max_n_steps: u32,
+    pub max_recursion_depth: usize,
+    pub chain_info: ChainInfo,
+}
+
+pub struct StatefulTransactionValidator {
+    config: StatefulTransactionValidatorConfig,
+}
+
+impl StatefulTransactionValidator {
+    pub fn new(config: StatefulTransactionValidatorConfig) -> Self {
+        Self { config }
+    }
+
+    pub fn run_validate<S: StateReader>(
+        &self,
+        // TODO(yael 17/4/24): the state_reader should be created inside the function taking
+        // latest_block_number.
+        state_reader: S,
+        //TODO(yael 17/4/24): the latest_block_info should be read from the storage.
+        latest_block_info: BlockInfo,
+        external_tx: &ExternalTransaction,
+        deploy_account_tx_hash: Option<TransactionHash>,
+        optional_class_info: Option<ClassInfo>,
+    ) -> StatefulTransactionValidatorResult<()> {
+        let state = CachedState::new(state_reader);
+        let versioned_constants = VersionedConstants::latest_constants_with_overrides(
+            self.config.validate_max_n_steps,
+            self.config.max_recursion_depth,
+        );
+        let mut block_info = latest_block_info;
+        block_info.block_number = block_info.block_number.next().ok_or(
+            StatefulTransactionValidatorError::BlockNumberOutOfRange {
+                block_number: block_info.block_number,
+            },
+        )?;
+        let block_context =
+            BlockContext::new_unchecked(&block_info, &self.config.chain_info, &versioned_constants);
+
+        let mut validator = BlockifierStatefulValidator::create(
+            state,
+            block_context,
+            self.config.max_nonce_for_validation_skip,
+            BouncerConfig::max(),
+        );
+        let account_tx = external_tx_to_account_tx(external_tx, optional_class_info)?;
+        validator.perform_validations(account_tx, deploy_account_tx_hash)?;
+        Ok(())
+    }
+}
---
+++ crates/gateway/src/utils.rs
@@ -0,0 +1,86 @@
+use crate::errors::StatefulTransactionValidatorResult;
+use blockifier::execution::contract_class::ClassInfo;
+use blockifier::transaction::account_transaction::AccountTransaction;
+use blockifier::transaction::transactions::DeclareTransaction as BlockifierDeclareTransaction;
+use blockifier::transaction::transactions::DeployAccountTransaction as BlockifierDeployAccountTransaction;
+use blockifier::transaction::transactions::InvokeTransaction as BlockifierInvokeTransaction;
+use starknet_api::core::{calculate_contract_address, ClassHash, ContractAddress};
+use starknet_api::external_transaction::{
+    ExternalDeclareTransaction, ExternalDeployAccountTransaction, ExternalInvokeTransaction,
+    ExternalTransaction,
+};
+use starknet_api::transaction::DeclareTransaction;
+use starknet_api::transaction::{
+    DeclareTransactionV3, DeployAccountTransaction, DeployAccountTransactionV3, InvokeTransaction,
+    InvokeTransactionV3, TransactionHash,
+};
+
+pub fn external_tx_to_account_tx(
+    external_tx: &ExternalTransaction,
+    optional_class_info: Option<ClassInfo>,
+) -> StatefulTransactionValidatorResult<AccountTransaction> {
+    let tx_hash = TransactionHash::default(); //TODO(yael 15/4/24): make TransactionHasher public in starknet-api
+    match external_tx {
+        ExternalTransaction::Declare(ExternalDeclareTransaction::V3(tx)) => {
+            let declare_tx = DeclareTransaction::V3(DeclareTransactionV3 {
+                class_hash: ClassHash::default(), //TODO(yael 15/4/24): call the starknet-api function once ready
+                resource_bounds: tx.resource_bounds.clone(),
+                tip: tx.tip,
+                signature: tx.signature.clone(),
+                nonce: tx.nonce,
+                compiled_class_hash: tx.compiled_class_hash,
+                sender_address: tx.sender_address,
+                nonce_data_availability_mode: tx.nonce_data_availability_mode,
+                fee_data_availability_mode: tx.fee_data_availability_mode,
+                paymaster_data: tx.paymaster_data.clone(),
+                account_deployment_data: tx.account_deployment_data.clone(),
+            });
+            let class_info =
+                optional_class_info.expect("declare transaction should contain class info");
+            let declare_tx = BlockifierDeclareTransaction::new(declare_tx, tx_hash, class_info)?;
+            Ok(AccountTransaction::Declare(declare_tx))
+        }
+        ExternalTransaction::DeployAccount(ExternalDeployAccountTransaction::V3(tx)) => {
+            let deploy_account_tx = DeployAccountTransaction::V3(DeployAccountTransactionV3 {
+                resource_bounds: tx.resource_bounds.clone(),
+                tip: tx.tip,
+                signature: tx.signature.clone(),
+                nonce: tx.nonce,
+                class_hash: tx.class_hash,
+                contract_address_salt: tx.contract_address_salt,
+                constructor_calldata: tx.constructor_calldata.clone(),
+                nonce_data_availability_mode: tx.nonce_data_availability_mode,
+                fee_data_availability_mode: tx.fee_data_availability_mode,
+                paymaster_data: tx.paymaster_data.clone(),
+            });
+            let contract_address = calculate_contract_address(
+                deploy_account_tx.contract_address_salt(),
+                deploy_account_tx.class_hash(),
+                &deploy_account_tx.constructor_calldata(),
+                ContractAddress::default(),
+            )?;
+            let deploy_account_tx = BlockifierDeployAccountTransaction::new(
+                deploy_account_tx,
+                tx_hash,
+                contract_address,
+            );
+            Ok(AccountTransaction::DeployAccount(deploy_account_tx))
+        }
+        ExternalTransaction::Invoke(ExternalInvokeTransaction::V3(tx)) => {
+            let invoke_tx = InvokeTransaction::V3(InvokeTransactionV3 {
+                resource_bounds: tx.resource_bounds.clone(),
+                tip: tx.tip,
+                signature: tx.signature.clone(),
+                nonce: tx.nonce,
+                sender_address: tx.sender_address,
+                calldata: tx.calldata.clone(),
+                nonce_data_availability_mode: tx.nonce_data_availability_mode,
+                fee_data_availability_mode: tx.fee_data_availability_mode,
+                paymaster_data: tx.paymaster_data.clone(),
+                account_deployment_data: tx.account_deployment_data.clone(),
+            });
+            let invoke_tx = BlockifierInvokeTransaction::new(invoke_tx, tx_hash);
+            Ok(AccountTransaction::Invoke(invoke_tx))
+        }
+    }
+}
