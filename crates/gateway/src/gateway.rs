114ace2a9936df2dd2099de546133baf0e7896a9
Arni Hod <arnon@starkware.co>
2024-04-11 16:24:36 +0300

    feat: stateless validations in add transaction

--- crates/gateway/src/gateway.rs
+++ crates/gateway/src/gateway.rs
@@ -1,3 +1,4 @@
+use axum::extract::State;
 use axum::http::StatusCode;
 use axum::response::{IntoResponse, Response};
 use axum::routing::{get, post};
@@ -6,6 +7,9 @@ use starknet_api::external_transaction::ExternalTransaction;
 use std::net::{IpAddr, SocketAddr};
 
 use crate::errors::GatewayError;
+use crate::stateless_transaction_validator::{
+    StatelessTransactionValidator, StatelessTransactionValidatorConfig,
+};
 
 pub enum GatewayResponse {
     ServerBuilt,
@@ -37,7 +41,7 @@ impl Gateway {
     pub async fn build_server(self) -> GatewayResult {
         // Parses the bind address from GatewayConfig, returning an error for invalid addresses.
         let addr = SocketAddr::new(self.config.ip, self.config.port);
-        let app = app();
+        let app = app(self.config);
 
         // Create a server that runs forever.
         axum::Server::bind(&addr)
@@ -50,10 +54,11 @@ impl Gateway {
 }
 
 /// Sets up the router with the specified routes for the server.
-pub fn app() -> Router {
+pub fn app(config: GatewayConfig) -> Router {
     Router::new()
         .route("/is_alive", get(is_alive))
         .route("/add_transaction", post(add_transaction))
+        .with_state(config)
     // TODO: when we need to configure the router, like adding banned ips, add it here via
     // `with_state`.
 }
@@ -62,7 +67,25 @@ async fn is_alive() -> impl IntoResponse {
     unimplemented!("Future handling should be implemented here.");
 }
 
-async fn add_transaction(Json(transaction): Json<ExternalTransaction>) -> GatewayResult {
+async fn add_transaction(
+    State(config): State<GatewayConfig>,
+    Json(transaction): Json<ExternalTransaction>,
+) -> GatewayResult {
+    // TODO(Arni, 1/5/2024): Preform congestion control.
+
+    // Perform stateless validations.
+    let stateless_validator = StatelessTransactionValidator {
+        config: config.stateless_transaction_validator_config,
+    };
+
+    stateless_validator.validate(&transaction)?;
+
+    // TODO(Arni, 1/5/2024): Descard duplications.
+    // TODO(Yael, 1/5/2024): Preform state related validations.
+    // TODO(Arni, 1/5/2024): Move transaction to mempool.
+
+    // TODO(Arni, 1/5/2024): Produce response.
+    // Send response.
     let positive_flow_response = match transaction {
         ExternalTransaction::Declare(_) => "DECLARE",
         ExternalTransaction::DeployAccount(_) => "DEPLOY_ACCOUNT",
@@ -71,7 +94,10 @@ async fn add_transaction(Json(transaction): Json<ExternalTransaction>) -> Gatewa
     Ok(GatewayResponse::TransactionAccepted(positive_flow_response))
 }
 
+#[derive(Clone)]
 pub struct GatewayConfig {
     pub ip: IpAddr,
     pub port: u16,
+
+    pub stateless_transaction_validator_config: StatelessTransactionValidatorConfig,
 }
