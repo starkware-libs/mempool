2fc052970351600ff5caa6f486cb4177754538aa
Arni Hod <arnon@starkware.co>
2024-04-11 16:24:36 +0300

    feat: stateless validations in add transaction

--- crates/gateway/src/errors.rs
+++ crates/gateway/src/errors.rs
@@ -14,6 +14,8 @@ pub enum GatewayError {
     InvalidTransactionFormat(#[from] serde_json::Error),
     #[error("Error while starting the server")]
     ServerStartError(#[from] hyper::Error),
+    #[error(transparent)]
+    StatelessTransactionValidatorError(#[from] StatelessTransactionValidatorError),
 }
 
 impl IntoResponse for GatewayError {
