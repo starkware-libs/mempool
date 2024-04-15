33445c04d623bc8a9bb0ba99b9d532e9d3346ff8
Uriel Korach <uriel@starkware.co>
2024-04-15 14:56:14 +0300

    feat: add a mempool proxy support for  concurrent calls

--- crates/mempool_node/src/mempool_proxy_test.rs
+++ crates/mempool_node/src/mempool_proxy_test.rs
@@ -1,18 +1,42 @@
 mod tests {
-    use std::sync::Arc;
 
-    use tokio::sync::Mutex;
+    use tokio::task::JoinSet;
 
     use crate::{
-        mempool::{Mempool, MempoolTrait},
+        mempool::{AddTransactionCallType, AddTransactionReturnType, MempoolTrait},
         mempool_proxy::MempoolProxy,
     };
 
     #[tokio::test]
-    async fn test_proxy_add_transaction() {
-        let mempool = Arc::new(Mutex::new(Mempool::new()));
-        let mut proxy = MempoolProxy::new(mempool);
-        assert_eq!(proxy.add_transaction(1).await, 1);
-        assert_eq!(proxy.add_transaction(1).await, 2);
+    async fn test_proxy_simple_add_transaction() {
+        let mut proxy = MempoolProxy::default();
+        let tx: AddTransactionCallType = 1;
+        let expect_result: AddTransactionReturnType = 1;
+        assert_eq!(proxy.add_transaction(tx).await, expect_result);
+    }
+
+    #[tokio::test]
+    async fn test_proxy_concurrent_add_transaction() {
+        let proxy = MempoolProxy::default();
+
+        let mut tasks: JoinSet<_> = (0..5)
+            .map(|_| {
+                let mut proxy = proxy.clone();
+                async move {
+                    let tx: AddTransactionCallType = 1;
+                    proxy.add_transaction(tx).await
+                }
+            })
+            .collect();
+
+        let mut results: Vec<AddTransactionReturnType> = vec![];
+        while let Some(result) = tasks.join_next().await {
+            results.push(result.unwrap());
+        }
+
+        results.sort();
+
+        let expected_results: Vec<AddTransactionReturnType> = (1..=5).collect();
+        assert_eq!(results, expected_results);
     }
 }
