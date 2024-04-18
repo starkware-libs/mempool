a3d748e44f045a9f243a43c72ec558accb180ec8
Yael Doweck <yael@starkware.co>
2024-04-18 09:21:48 +0300

    feat: rpc state reader

---
+++ crates/gateway/src/rpc_objects.rs
@@ -0,0 +1,56 @@
+use serde::{Deserialize, Serialize};
+use starknet_api::core::ContractAddress;
+
+// Starknet Spec error codes:
+// TODO(yael 30/4/2024): consider turning these into an enum.
+pub const RPC_ERROR_BLOCK_NOT_FOUND: u16 = 24;
+pub const RPC_ERROR_CONTRACT_ADDRESS_NOT_FOUND: u16 = 20;
+
+#[derive(Deserialize, Serialize)]
+pub enum BlockTag {
+    /// The most recent fully constructed block
+    #[serde(rename = "latest")]
+    Latest,
+    /// Currently constructed block
+    #[serde(rename = "pending")]
+    Pending,
+}
+
+#[derive(Deserialize, Serialize)]
+#[serde(untagged)]
+pub enum BlockId {
+    Tag(BlockTag),
+    // There are additional options in the spec that are not implemented here
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct GetNonceParams {
+    pub block_id: BlockId,
+    pub contract_address: ContractAddress,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(untagged)]
+pub enum RpcResponse {
+    Success(RpcSuccessResponse),
+    Error(RpcErrorResponse),
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct RpcSuccessResponse {
+    pub jsonrpc: Option<String>,
+    pub result: String,
+    pub id: u32,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct RpcErrorResponse {
+    pub jsonrpc: Option<String>,
+    pub error: RpcError,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct RpcError {
+    pub code: u16,
+    pub message: String,
+}
