3884f13bb9c037b6e8f9de69d54094e267314915
Yael Doweck <yael@starkware.co>
2024-05-02 09:32:38 +0300

    feat: implement get_class_hash_at for rpc state reader

--- crates/gateway/src/rpc_state_reader_test.rs
+++ crates/gateway/src/rpc_state_reader_test.rs
@@ -11,12 +11,13 @@ use starknet_api::{contract_address, patricia_key};
 
 use crate::rpc_objects::BlockId;
 use crate::rpc_objects::BlockTag;
+use crate::rpc_objects::GetClassHashAtParams;
 use crate::rpc_objects::GetNonceParams;
 use crate::rpc_objects::GetStorageAtParams;
 use crate::rpc_state_reader::RpcStateReader;
 use url::Url;
 
-const STARKNET_SPEC_VERSION: &str = "v0_6";
+const STARKNET_SPEC_VERSION: &str = "v0_7";
 const JSON_RPC_VERSION: &str = "2.0";
 const RPC_URL_MAINNET: &str = "https://papyrus-for-mempool-mainnet.sw-dev.io/rpc/";
 // TODO(yael 1/5/2024): test also sepolia and sepolia-integration
@@ -28,15 +29,19 @@ const _RPC_URL_INTEGRATION_SEPOLIA: &str =
 const VALID_CONTRACT_ADDRESS: &str =
     "0x0240edc989dfc8b4d75d7f6fa6f8a48e7ff4af358c6dd72e4b3cb67687b204e0";
 
-#[test]
-fn test_rpc_get_nonce() {
-    let state_reader = RpcStateReader {
+fn state_reader() -> RpcStateReader {
+    RpcStateReader {
         url: Url::parse(RPC_URL_MAINNET)
             .unwrap()
             .join(STARKNET_SPEC_VERSION)
             .unwrap(),
         json_rpc_version: JSON_RPC_VERSION.to_string(),
-    };
+    }
+}
+
+#[test]
+fn test_rpc_get_nonce() {
+    let state_reader = state_reader();
     // Query with a valid address
     let contract_address = contract_address!(VALID_CONTRACT_ADDRESS);
 
@@ -69,13 +74,7 @@ fn test_rpc_get_nonce() {
 
 #[test]
 fn test_rpc_get_storage_at() {
-    let state_reader = RpcStateReader {
-        url: Url::parse(RPC_URL_MAINNET)
-            .unwrap()
-            .join(STARKNET_SPEC_VERSION)
-            .unwrap(),
-        json_rpc_version: JSON_RPC_VERSION.to_string(),
-    };
+    let state_reader = state_reader();
     // Query with a valid address
     let contract_address = contract_address!(VALID_CONTRACT_ADDRESS);
 
@@ -97,6 +96,39 @@ fn test_rpc_get_storage_at() {
                 block_id: BlockId::Tag(BlockTag::Latest),
                 contract_address,
                 key: StorageKey(patricia_key!("0x0")),
+            })}
+        )
+    );
+    assert_matches!(
+        res,
+        Err(StateError::StateReadError(state_reader_error))
+        if state_reader_error == expected_error
+    );
+}
+
+#[test]
+fn test_rpc_get_class_hash_at() {
+    let state_reader = state_reader();
+    // Query with a valid address
+    let contract_address = contract_address!(VALID_CONTRACT_ADDRESS);
+
+    let res = state_reader.get_class_hash_at(contract_address);
+    assert!(res.is_ok());
+
+    // Query with an invalid address
+    let contract_address = contract_address!("0x0");
+
+    let res = state_reader.get_class_hash_at(contract_address);
+
+    let expected_error = format!(
+        "Contract address not found, request: {}",
+        json!({
+            "jsonrpc": JSON_RPC_VERSION,
+            "id": 0,
+            "method": "starknet_getClassHashAt",
+            "params": json!(GetClassHashAtParams {
+                block_id: BlockId::Tag(BlockTag::Latest),
+                contract_address,
             })}
         )
     );
