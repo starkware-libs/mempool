9765c08b0e3be66957fb1190b87dee9abca53680
Arni Hod <arnon@starkware.co>
2024-05-07 13:53:38 +0300

    chore: rename gateway config to gateway network config

--- crates/gateway/src/config_test.rs
+++ crates/gateway/src/config_test.rs
@@ -1,4 +1,4 @@
-use crate::config::GatewayConfig;
+use crate::config::GatewayNetworkConfig;
 
 use clap::Command;
 
@@ -8,22 +8,22 @@ use std::path::Path;
 use validator::Validate;
 
 const TEST_FILES_FOLDER: &str = "./src/json_files_for_testing";
-const CONFIG_FILE: &str = "gateway_config.json";
+const NETWORK_CONFIG_FILE: &str = "gateway_network_config.json";
 
-fn get_config_file(file_name: &str) -> Result<GatewayConfig, papyrus_config::ConfigError> {
+fn get_config_file(file_name: &str) -> Result<GatewayNetworkConfig, papyrus_config::ConfigError> {
     let config_file = File::open(Path::new(TEST_FILES_FOLDER).join(file_name)).unwrap();
-    load_and_process_config::<GatewayConfig>(config_file, Command::new(""), vec![])
+    load_and_process_config::<GatewayNetworkConfig>(config_file, Command::new(""), vec![])
 }
 
 #[test]
 fn test_valid_config() {
     // Read the valid config file and validate its content.
-    let expected_config = GatewayConfig {
+    let expected_config = GatewayNetworkConfig {
         ip: "0.0.0.0".parse().unwrap(),
         port: 8080,
     };
 
-    let loaded_config = get_config_file(CONFIG_FILE).unwrap();
+    let loaded_config = get_config_file(NETWORK_CONFIG_FILE).unwrap();
 
     assert!(loaded_config.validate().is_ok());
     assert_eq!(loaded_config, expected_config);
