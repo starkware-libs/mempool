0fc31c4482fd56d2e5dcd806a45013fde6edcd5f
Arni Hod <arnon@starkware.co>
2024-05-08 11:17:41 +0300

    test: generazlie test_valid_config test util

--- crates/gateway/src/config_test.rs
+++ crates/gateway/src/config_test.rs
@@ -2,6 +2,7 @@ use clap::Command;
 use papyrus_config::dumping::SerializeConfig;
 use papyrus_config::loading::load_and_process_config;
 use serde::Deserialize;
+use std::fmt::Debug;
 use std::fs::File;
 use std::path::{Path, PathBuf};
 use validator::Validate;
@@ -15,34 +16,36 @@ fn get_config_file_path() -> PathBuf {
     Path::new(TEST_FILES_FOLDER).join(CONFIG_FILE)
 }
 
-fn get_config_from_file<T: for<'a> Deserialize<'a>>(
-    file_path: PathBuf,
-) -> Result<T, papyrus_config::ConfigError> {
-    let config_file = File::open(file_path).unwrap();
-    load_and_process_config(config_file, Command::new(""), vec![])
-}
-
-/// Read the valid config file and validate its content.
-fn test_valid_config_body(fix: bool) {
-    let expected_config = GatewayNetworkConnectionConfig {
-        ip: "0.0.0.0".parse().unwrap(),
-        port: 8080,
-    };
-
-    let file_path = get_config_file_path();
+fn test_valid_config_body<
+    T: for<'a> Deserialize<'a> + SerializeConfig + Validate + PartialEq + Debug,
+>(
+    expected_config: T,
+    config_file_path: PathBuf,
+    fix: bool,
+) {
     if fix {
         expected_config
-            .dump_to_file(&vec![], file_path.to_str().unwrap())
+            .dump_to_file(&vec![], config_file_path.to_str().unwrap())
             .unwrap();
     }
-    let loaded_config = get_config_from_file::<GatewayNetworkConnectionConfig>(file_path).unwrap();
+
+    let config_file = File::open(config_file_path).unwrap();
+    let loaded_config =
+        load_and_process_config::<T>(config_file, Command::new(""), vec![]).unwrap();
 
     assert!(loaded_config.validate().is_ok());
     assert_eq!(loaded_config, expected_config);
 }
 
 #[test]
+/// Read the valid config file and validate its content.
 fn test_valid_config() {
+    let expected_config = GatewayNetworkConnectionConfig {
+        ip: "0.0.0.0".parse().unwrap(),
+        port: 8080,
+    };
+    let file_path = get_config_file_path();
     // To fix the test, set the `fix` parameter to `true`, and run the test.
-    test_valid_config_body(false);
+    let fix = false;
+    test_valid_config_body(expected_config, file_path, fix);
 }
