5cc48aaec47de99a1544f99f8c205c9d372426a7
Arni Hod <arnon@starkware.co>
2024-05-08 16:14:41 +0300

    test: generazlie test_valid_config test util

--- crates/gateway/src/config_test.rs
+++ crates/gateway/src/config_test.rs
@@ -2,6 +2,7 @@ use clap::Command;
 use papyrus_config::dumping::SerializeConfig;
 use papyrus_config::loading::load_and_process_config;
 use serde::Deserialize;
+use std::fmt::Debug;
 use std::fs::File;
 use std::path::{Path, PathBuf};
 use validator::Validate;
@@ -22,28 +23,35 @@ fn get_config_from_file<T: for<'a> Deserialize<'a>>(
     load_and_process_config(config_file, Command::new(""), vec![])
 }
 
-/// Read the valid config file and validate its content.
-fn test_valid_network_config_body(fix: bool) {
-    let expected_config = GatewayNetworkConfig {
-        ip: "0.0.0.0".parse().unwrap(),
-        port: 8080,
-    };
-
-    let file_path = get_config_file_path(NETWORK_CONFIG_FILE);
+fn test_valid_config_body<
+    T: for<'a> Deserialize<'a> + SerializeConfig + Validate + PartialEq + Debug,
+>(
+    expected_config: T,
+    config_file_path: PathBuf,
+    fix: bool,
+) {
     if fix {
         expected_config
-            .dump_to_file(&vec![], file_path.to_str().unwrap())
+            .dump_to_file(&vec![], config_file_path.to_str().unwrap())
             .unwrap();
     }
-    let loaded_config = get_config_from_file::<GatewayNetworkConfig>(file_path).unwrap();
+
+    let loaded_config: T = get_config_from_file(config_file_path).unwrap();
 
     assert!(loaded_config.validate().is_ok());
     assert_eq!(loaded_config, expected_config);
 }
 
 #[test]
+/// Read the valid config file and validate its content.
 fn test_valid_config() {
+    let expected_config = GatewayNetworkConfig {
+        ip: "0.0.0.0".parse().unwrap(),
+        port: 8080,
+    };
+    let file_path = get_config_file_path(NETWORK_CONFIG_FILE);
     // TODO(Arni, 1/7/2024): Create a test fix feature. See:
     // https://users.rust-lang.org/t/how-do-i-run-only-a-specific-file-other-than-main-rs/76213/3
-    test_valid_network_config_body(false);
+    let fix = false;
+    test_valid_config_body(expected_config, file_path, fix);
 }
