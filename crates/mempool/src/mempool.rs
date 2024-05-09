78a5166472610678386cd51e15d44cd1f5cd3769
Mohammad Nassar <mohammad@starkware.co>
2024-05-09 13:04:16 +0300

    feat: listen for requests from a network for the mempool

--- crates/mempool/src/mempool.rs
+++ crates/mempool/src/mempool.rs
@@ -1,11 +1,15 @@
 use std::collections::hash_map::Entry::{Occupied, Vacant};
 use std::collections::HashMap;
 
+use anyhow::Result;
+use mempool_infra::network_component::CommunicationInterface;
 use starknet_api::core::ContractAddress;
 use starknet_api::transaction::TransactionHash;
 use starknet_mempool_types::mempool_types::{
-    Account, AccountState, MempoolInput, MempoolNetworkComponent, ThinTransaction,
+    Account, AccountState, GatewayToMempoolMessage, MempoolInput, MempoolNetworkComponent,
+    ThinTransaction,
 };
+use tokio::select;
 
 use crate::errors::MempoolError;
 use crate::priority_queue::PriorityQueue;
@@ -88,5 +92,33 @@ impl Mempool {
         _state_changes: HashMap<ContractAddress, AccountState>,
     ) -> MempoolResult<()> {
         todo!()
+    }
+
+    /// Listens asynchronously for network messages and processes them.
+    pub async fn run(&mut self) -> Result<String> {
+        loop {
+            select! {
+                optional_message = self.network.recv() => {
+                    match optional_message {
+                        Some(message) => {
+                            self.process_network_message(message)?;
+                        },
+                        // Channel was closed; exit.
+                        None => break,
+                    }
+                }
+            }
+        }
+        Ok("Exiting the loop successfully".to_string())
+    }
+
+    /// Processes a single message received from the network.
+    fn process_network_message(&mut self, message: GatewayToMempoolMessage) -> Result<String> {
+        match message {
+            GatewayToMempoolMessage::AddTransaction(tx, account_state) => {
+                self.add_tx(tx, account_state)?;
+                Ok("Transaction added successfully".to_string())
+            }
+        }
     }
 }
