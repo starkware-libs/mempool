9db7bdde878be2981b037d23aeae86516c60a1cb
Arni Hod <arnon@starkware.co>
2024-05-12 14:53:36 +0300

    test: streamline the fix infra for config tests

--- crates/gateway/src/config_test.rs
+++ crates/gateway/src/config_test.rs
@@ -3,6 +3,7 @@ use std::fs::File;
 use std::path::{Path, PathBuf};
 
 use clap::Command;
+use expect_test::expect_file;
 use papyrus_config::dumping::SerializeConfig;
 use papyrus_config::loading::load_and_process_config;
 use serde::Deserialize;
@@ -11,13 +12,10 @@ use validator::Validate;
 use crate::config::GatewayNetworkConfig;
 use crate::stateless_transaction_validator::StatelessTransactionValidatorConfig;
 
-const TEST_FILES_FOLDER: &str = "./src/json_files_for_testing";
+const TEST_FILES_FOLDER: &str = "json_files_for_testing";
+const GATEWAY_SOURCE_FOLDER: &str = "./src";
 const NETWORK_CONFIG_FILE: &str = "gateway_network_config.json";
 const STATELESS_TRANSACTION_VALIDATOR_CONFIG: &str = "stateless_transaction_validator_config.json";
-
-fn get_config_file_path(file_name: &str) -> PathBuf {
-    Path::new(TEST_FILES_FOLDER).join(file_name)
-}
 
 fn get_config_from_file<T: for<'a> Deserialize<'a>>(
     file_path: PathBuf,
@@ -29,42 +27,36 @@ fn get_config_from_file<T: for<'a> Deserialize<'a>>(
 fn test_valid_config_body<
     T: for<'a> Deserialize<'a> + SerializeConfig + Validate + PartialEq + Debug,
 >(
-    expected_config: T,
-    config_file_path: PathBuf,
-    fix: bool,
+    config_struct: T,
+    file_name: &str,
 ) {
-    if fix {
-        expected_config.dump_to_file(&vec![], config_file_path.to_str().unwrap()).unwrap();
-    }
+    let path = Path::new(TEST_FILES_FOLDER).join(file_name);
+    // Test seralize.
+    let expected_file_content = expect_file![path.to_str().unwrap()];
+    let give_file_content = serde_json::to_string_pretty(&config_struct.dump()).unwrap();
+    expected_file_content.assert_eq(&give_file_content);
 
-    let loaded_config: T = get_config_from_file(config_file_path).unwrap();
+    // Test deserialize.
+    let path = Path::new(GATEWAY_SOURCE_FOLDER).join(path);
+    // let file_path_to_load = Path::new(TEST_FILES_FOLDER).join(file_name);
+    let loaded_config: T = get_config_from_file(path).unwrap();
 
+    // Validate the loaded config.
     assert!(loaded_config.validate().is_ok());
-    assert_eq!(loaded_config, expected_config);
+    assert_eq!(loaded_config, config_struct);
 }
 
 #[test]
 /// Read the network config file and validate its content.
+/// Fix with "env UPDATE_EXPECT=1 cargo test"
 fn test_valid_network_config() {
     let expected_config = GatewayNetworkConfig { ip: "0.0.0.0".parse().unwrap(), port: 8080 };
-    let file_path = get_config_file_path(NETWORK_CONFIG_FILE);
-    let fix = false;
-    test_valid_config_body(expected_config, file_path, fix);
-}
-
-// TODO(Arni, 7/5/2024): Dedup code with test_valid_config.
-#[test]
-#[ignore]
-/// Fix the config file for test_valid_network_config. Run with 'cargo test -- --ignored'.
-fn fix_test_valid_config() {
-    let expected_config = GatewayNetworkConfig { ip: "0.0.0.0".parse().unwrap(), port: 8080 };
-    let file_path = get_config_file_path(NETWORK_CONFIG_FILE);
-    let fix = true;
-    test_valid_config_body(expected_config, file_path, fix);
+    test_valid_config_body(expected_config, NETWORK_CONFIG_FILE);
 }
 
 #[test]
 /// Read the stateless transaction validator config file and validate its content.
+/// fix with "env UPDATE_EXPECT=1 cargo test"
 fn test_valid_stateless_transaction_validator_config() {
     let expected_config = StatelessTransactionValidatorConfig {
         validate_non_zero_l1_gas_fee: true,
@@ -72,23 +64,5 @@ fn test_valid_stateless_transaction_validator_config() {
         max_calldata_length: 10,
         max_signature_length: 0,
     };
-    let file_path = get_config_file_path(STATELESS_TRANSACTION_VALIDATOR_CONFIG);
-    let fix = false;
-    test_valid_config_body(expected_config, file_path, fix);
-}
-
-#[test]
-#[ignore]
-/// Fix the config file for test_valid_stateless_transaction_validator_config.
-/// Run with 'cargo test -- --ignored'.
-fn fix_test_valid_stateless_transaction_validator_config() {
-    let expected_config = StatelessTransactionValidatorConfig {
-        validate_non_zero_l1_gas_fee: true,
-        validate_non_zero_l2_gas_fee: false,
-        max_calldata_length: 10,
-        max_signature_length: 0,
-    };
-    let file_path = get_config_file_path(STATELESS_TRANSACTION_VALIDATOR_CONFIG);
-    let fix = true;
-    test_valid_config_body(expected_config, file_path, fix);
+    test_valid_config_body(expected_config, STATELESS_TRANSACTION_VALIDATOR_CONFIG);
 }
