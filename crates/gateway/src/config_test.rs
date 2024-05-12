884cda92be490bb8e5971ae1f6ae2a25847f0c93
Arni Hod <arnon@starkware.co>
2024-05-12 14:53:36 +0300

    test: streamline the fix infra for config tests

--- crates/gateway/src/config_test.rs
+++ crates/gateway/src/config_test.rs
@@ -1,8 +1,9 @@
 use std::fmt::Debug;
 use std::fs::File;
-use std::path::{Path, PathBuf};
+use std::path::Path;
 
 use clap::Command;
+use expect_test::{expect_file, ExpectFile};
 use papyrus_config::dumping::SerializeConfig;
 use papyrus_config::loading::load_and_process_config;
 use serde::Deserialize;
@@ -12,105 +13,67 @@ use crate::config::{
     GatewayNetworkConfig, RpcStateReaderConfig, StatelessTransactionValidatorConfig,
 };
 
-const TEST_FILES_FOLDER: &str = "./src/json_files_for_testing";
-const NETWORK_CONFIG_FILE: &str = "gateway_network_config.json";
+const TEST_FILES_FOLDER: &str = "json_files_for_testing";
+const GATEWAY_SOURCE_FOLDER: &str = "./src";
+
+const NETWORK_CONFIG: &str = "gateway_network_config.json";
 const STATELESS_TRANSACTION_VALIDATOR_CONFIG: &str = "stateless_transaction_validator_config.json";
 const RPC_STATE_READER_CONFIG: &str = "rpc_state_reader_config.json";
 
-fn get_config_file_path(file_name: &str) -> PathBuf {
-    Path::new(TEST_FILES_FOLDER).join(file_name)
-}
-
-fn get_config_from_file<T: for<'a> Deserialize<'a>>(
-    file_path: PathBuf,
-) -> Result<T, papyrus_config::ConfigError> {
-    let config_file = File::open(file_path).unwrap();
-    load_and_process_config(config_file, Command::new(""), vec![])
-}
-
-fn test_valid_config_body<
-    T: for<'a> Deserialize<'a> + SerializeConfig + Validate + PartialEq + Debug,
->(
-    expected_config: T,
-    config_file_path: PathBuf,
-    fix: bool,
+/// Tests the basic functionality of Papyrus config files.
+/// Tests the seriazlization of a config struct by calling the 'dump' method, and the
+/// deserialization of a config .json file by calling the 'load_and_process_config' function.
+/// Finally, it validates the config struct.
+fn config_test_body<T: for<'a> Deserialize<'a> + SerializeConfig + Validate + PartialEq + Debug>(
+    config_struct: T,
+    serialized_config_file_name: &str,
 ) {
-    if fix {
-        expected_config.dump_to_file(&vec![], config_file_path.to_str().unwrap()).unwrap();
-    }
+    // Test serialize.
 
-    let loaded_config: T = get_config_from_file(config_file_path).unwrap();
+    let serialized_struct = serde_json::to_string_pretty(&config_struct.dump()).unwrap();
+    // The macro `expect_file!` requires the relative path to the file.
+    let path = Path::new(TEST_FILES_FOLDER).join(serialized_config_file_name);
+    let expected_serialized_struct: ExpectFile = expect_file![path.to_str().unwrap()];
+    // If the environment variable `UPDATE_EXPECT` is set, the expected file will be updated.
+    expected_serialized_struct.assert_eq(&serialized_struct);
 
-    assert!(loaded_config.validate().is_ok());
-    assert_eq!(loaded_config, expected_config);
+    // Test deserialize.
+
+    // The function `load_and_process_config` requires the absolute path (from the crate) to the
+    // file.
+    let path = Path::new(GATEWAY_SOURCE_FOLDER).join(path);
+    let config_file = File::open(path).unwrap();
+    let loaded_config: T = load_and_process_config(config_file, Command::new(""), vec![]).unwrap();
+    assert_eq!(loaded_config, config_struct);
+
+    // Validate the loaded config.
+    assert!(config_struct.validate().is_ok());
 }
 
 #[test]
 /// Read the network config file and validate its content.
-fn test_valid_network_config() {
-    let expected_config = GatewayNetworkConfig { ip: "0.0.0.0".parse().unwrap(), port: 8080 };
-    let file_path = get_config_file_path(NETWORK_CONFIG_FILE);
-    let fix = false;
-    test_valid_config_body(expected_config, file_path, fix);
-}
-
-// TODO(Arni, 7/5/2024): Dedup code with test_valid_config.
-#[test]
-#[ignore]
-/// Fix the config file for test_valid_network_config. Run with 'cargo test -- --ignored'.
-fn fix_test_valid_network_config() {
-    let expected_config = GatewayNetworkConfig { ip: "0.0.0.0".parse().unwrap(), port: 8080 };
-    let file_path = get_config_file_path(NETWORK_CONFIG_FILE);
-    let fix = true;
-    test_valid_config_body(expected_config, file_path, fix);
+/// Fix with "env UPDATE_EXPECT=1 cargo test"
+fn test_network_config() {
+    let config_struct = GatewayNetworkConfig { ip: "0.0.0.0".parse().unwrap(), port: 8080 };
+    config_test_body(config_struct, NETWORK_CONFIG);
 }
 
 #[test]
 /// Read the stateless transaction validator config file and validate its content.
-fn test_valid_stateless_transaction_validator_config() {
-    let expected_config = StatelessTransactionValidatorConfig {
+/// fix with "env UPDATE_EXPECT=1 cargo test"
+fn test_stateless_transaction_validator_config() {
+    let config_struct = StatelessTransactionValidatorConfig {
         validate_non_zero_l1_gas_fee: true,
         validate_non_zero_l2_gas_fee: false,
         max_calldata_length: 10,
         max_signature_length: 0,
     };
-    let file_path = get_config_file_path(STATELESS_TRANSACTION_VALIDATOR_CONFIG);
-    let fix = false;
-    test_valid_config_body(expected_config, file_path, fix);
-}
-
-#[test]
-#[ignore]
-/// Fix the config file for test_valid_stateless_transaction_validator_config.
-/// Run with 'cargo test -- --ignored'.
-fn fix_test_valid_stateless_transaction_validator_config() {
-    let expected_config = StatelessTransactionValidatorConfig {
-        validate_non_zero_l1_gas_fee: true,
-        validate_non_zero_l2_gas_fee: false,
-        max_calldata_length: 10,
-        max_signature_length: 0,
-    };
-    let file_path = get_config_file_path(STATELESS_TRANSACTION_VALIDATOR_CONFIG);
-    let fix = true;
-    test_valid_config_body(expected_config, file_path, fix);
+    config_test_body(config_struct, STATELESS_TRANSACTION_VALIDATOR_CONFIG);
 }
 
 #[test]
 /// Read the rpc state reader config file and validate its content.
 fn test_valid_rpc_state_reader_config() {
-    let expected_config = RpcStateReaderConfig::create_for_testing();
-    let file_path = get_config_file_path(RPC_STATE_READER_CONFIG);
-    let fix = false;
-    test_valid_config_body(expected_config, file_path, fix);
-}
-
-#[test]
-#[ignore]
-/// Fix the config file for test_valid_rpc_state_reader_config.
-/// Run with 'cargo test -- --ignored'.
-fn fix_test_valid_rpc_state_reader_config() {
-    let expected_config = RpcStateReaderConfig::create_for_testing();
-    let file_path = get_config_file_path(RPC_STATE_READER_CONFIG);
-    let fix = true;
-    test_valid_config_body(expected_config, file_path, fix);
+    let config_struct = RpcStateReaderConfig::create_for_testing();
+    config_test_body(config_struct, RPC_STATE_READER_CONFIG);
 }
