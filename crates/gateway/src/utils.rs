3d08f128297cd8de5bf55aaa0ffa419e84f8a073
Ayelet Zilber <ayelet@starkware.co>
2024-05-15 14:00:35 +0300

    refactor: replace invoke json file with macro creation

--- crates/gateway/src/utils.rs
+++ crates/gateway/src/utils.rs
@@ -5,15 +5,19 @@ use blockifier::transaction::transactions::{
     DeployAccountTransaction as BlockifierDeployAccountTransaction,
     InvokeTransaction as BlockifierInvokeTransaction,
 };
-use starknet_api::core::{calculate_contract_address, ChainId, ClassHash, ContractAddress};
+use serde_json::{to_string_pretty, Value};
+use starknet_api::calldata;
+use starknet_api::core::{calculate_contract_address, ChainId, ClassHash, ContractAddress, Nonce};
+use starknet_api::data_availability::DataAvailabilityMode;
 use starknet_api::external_transaction::{
     ExternalDeclareTransaction, ExternalDeployAccountTransaction, ExternalInvokeTransaction,
-    ExternalTransaction,
+    ExternalInvokeTransactionV3, ExternalTransaction,
 };
 use starknet_api::transaction::{
-    DeclareTransaction, DeclareTransactionV3, DeployAccountTransaction, DeployAccountTransactionV3,
-    InvokeTransaction, InvokeTransactionV3, ResourceBoundsMapping, TransactionHasher,
-    TransactionSignature,
+    AccountDeploymentData, Calldata, DeclareTransaction, DeclareTransactionV3,
+    DeployAccountTransaction, DeployAccountTransactionV3, InvokeTransaction, InvokeTransactionV3,
+    PaymasterData, Resource, ResourceBounds, ResourceBoundsMapping, Tip, TransactionHasher,
+    TransactionSignature, TransactionVersion,
 };
 
 use crate::errors::StatefulTransactionValidatorResult;
@@ -123,4 +127,95 @@ pub fn external_tx_to_account_tx(
             Ok(AccountTransaction::Invoke(invoke_tx))
         }
     }
+}
+
+#[derive(Clone)]
+pub struct InvokeTxArgs {
+    pub signature: TransactionSignature,
+    pub contract_address: ContractAddress,
+    pub calldata: Calldata,
+    pub version: TransactionVersion,
+    pub resource_bounds: ResourceBoundsMapping,
+    pub tip: Tip,
+    pub nonce_data_availability_mode: DataAvailabilityMode,
+    pub fee_data_availability_mode: DataAvailabilityMode,
+    pub paymaster_data: PaymasterData,
+    pub account_deployment_data: AccountDeploymentData,
+    pub nonce: Nonce,
+}
+impl Default for InvokeTxArgs {
+    fn default() -> Self {
+        InvokeTxArgs {
+            signature: TransactionSignature::default(),
+            contract_address: ContractAddress::default(),
+            calldata: calldata![],
+            version: TransactionVersion::THREE,
+            resource_bounds: ResourceBoundsMapping::try_from(vec![
+                (Resource::L1Gas, ResourceBounds { max_amount: 0, max_price_per_unit: 1 }),
+                (Resource::L2Gas, ResourceBounds { max_amount: 0, max_price_per_unit: 0 }),
+            ])
+            .unwrap(),
+            tip: Tip::default(),
+            nonce_data_availability_mode: DataAvailabilityMode::L1,
+            fee_data_availability_mode: DataAvailabilityMode::L1,
+            paymaster_data: PaymasterData::default(),
+            account_deployment_data: AccountDeploymentData::default(),
+            nonce: Nonce::default(),
+        }
+    }
+}
+
+// TODO(Ayelet, 15/5/2025): Move this to StarkNet API.
+#[macro_export]
+macro_rules! invoke_tx_args {
+    ($($field:ident $(: $value:expr)?),* $(,)?) => {
+        $crate::utils::InvokeTxArgs {
+            $($field $(: $value)?,)*
+            ..Default::default()
+        }
+    };
+    ($($field:ident $(: $value:expr)?),* , ..$defaults:expr) => {
+        $crate::utils::InvokeTxArgs {
+            $($field $(: $value)?,)*
+            ..$defaults
+        }
+    };
+}
+
+pub fn external_invoke_tx(invoke_args: InvokeTxArgs) -> ExternalInvokeTransaction {
+    match invoke_args.version {
+        TransactionVersion::THREE => {
+            starknet_api::external_transaction::ExternalInvokeTransaction::V3(
+                ExternalInvokeTransactionV3 {
+                    resource_bounds: invoke_args.resource_bounds,
+                    tip: invoke_args.tip,
+                    calldata: invoke_args.calldata,
+                    sender_address: invoke_args.contract_address,
+                    nonce: invoke_args.nonce,
+                    signature: invoke_args.signature,
+                    nonce_data_availability_mode: invoke_args.nonce_data_availability_mode,
+                    fee_data_availability_mode: invoke_args.fee_data_availability_mode,
+                    paymaster_data: invoke_args.paymaster_data,
+                    account_deployment_data: invoke_args.account_deployment_data,
+                },
+            )
+        }
+        _ => panic!("Unsupported transaction version: {:?}.", invoke_args.version),
+    }
+}
+
+pub fn external_invoke_tx_to_json(tx: ExternalTransaction) -> String {
+    // Serialize to JSON
+    let mut tx_json = serde_json::to_value(&tx).expect("Failed to serialize transaction");
+
+    // Add type and version manually
+    if let Value::Object(ref mut map) = tx_json {
+        map.insert("type".to_string(), Value::String("INVOKE_FUNCTION".to_string()));
+    }
+    if let Value::Object(ref mut map) = tx_json {
+        map.insert("version".to_string(), Value::String("0x3".to_string()));
+    }
+
+    // Serialize back to pretty JSON string
+    to_string_pretty(&tx_json).expect("Failed to serialize transaction")
 }
