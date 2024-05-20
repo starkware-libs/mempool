da2d0517f23fb7cef9f25ab2a50a35a27c477262
Arni Hod <arnon@starkware.co>
2024-05-20 16:19:16 +0300

    test: remove redundent config tests

--- crates/gateway/src/config_test.rs
+++ crates/gateway/src/config_test.rs
@@ -1,51 +1,9 @@
-use std::fmt::Debug;
-use std::fs::File;
-use std::path::{Path, PathBuf};
-
-use clap::Command;
-use papyrus_config::dumping::SerializeConfig;
-use papyrus_config::loading::load_and_process_config;
 use rstest::{fixture, rstest};
-use serde::Deserialize;
 use validator::Validate;
 
 use crate::config::{
     GatewayConfig, GatewayNetworkConfig, RpcStateReaderConfig, StatelessTransactionValidatorConfig,
 };
-
-const TEST_FILES_FOLDER: &str = "./src/json_files_for_testing";
-const NETWORK_CONFIG_FILE: &str = "gateway_network_config.json";
-const STATELESS_TRANSACTION_VALIDATOR_CONFIG: &str = "stateless_transaction_validator_config.json";
-const RPC_STATE_READER_CONFIG: &str = "rpc_state_reader_config.json";
-const GATEWAY_CONFIG_FILE: &str = "gateway_config.json";
-
-fn get_config_file_path(file_name: &str) -> PathBuf {
-    Path::new(TEST_FILES_FOLDER).join(file_name)
-}
-
-fn get_config_from_file<T: for<'a> Deserialize<'a>>(
-    file_path: PathBuf,
-) -> Result<T, papyrus_config::ConfigError> {
-    let config_file = File::open(file_path).unwrap();
-    load_and_process_config(config_file, Command::new(""), vec![])
-}
-
-fn test_valid_config_body<
-    T: for<'a> Deserialize<'a> + SerializeConfig + Validate + PartialEq + Debug,
->(
-    expected_config: T,
-    config_file_path: PathBuf,
-    fix: bool,
-) {
-    if fix {
-        expected_config.dump_to_file(&vec![], config_file_path.to_str().unwrap()).unwrap();
-    }
-
-    let loaded_config: T = get_config_from_file(config_file_path).unwrap();
-
-    assert!(loaded_config.validate().is_ok());
-    assert_eq!(loaded_config, expected_config);
-}
 
 #[fixture]
 fn gateway_network_config() -> GatewayNetworkConfig {
@@ -79,21 +37,8 @@ fn gateway_config(
 }
 
 #[rstest]
-/// Read the network config file and validate its content.
 fn test_valid_network_config(gateway_network_config: GatewayNetworkConfig) {
-    let file_path = get_config_file_path(NETWORK_CONFIG_FILE);
-    let fix = false;
-    test_valid_config_body(gateway_network_config, file_path, fix);
-}
-
-// TODO(Arni, 7/5/2024): Dedup code with test_valid_config.
-#[rstest]
-#[ignore]
-/// Fix the config file for test_valid_network_config. Run with 'cargo test -- --ignored'.
-fn fix_test_valid_network_config(gateway_network_config: GatewayNetworkConfig) {
-    let file_path = get_config_file_path(NETWORK_CONFIG_FILE);
-    let fix = true;
-    test_valid_config_body(gateway_network_config, file_path, fix);
+    assert!(gateway_network_config.validate().is_ok());
 }
 
 #[rstest]
@@ -101,55 +46,17 @@ fn fix_test_valid_network_config(gateway_network_config: GatewayNetworkConfig) {
 fn test_valid_stateless_transaction_validator_config(
     stateless_transaction_validator_config: StatelessTransactionValidatorConfig,
 ) {
-    let file_path = get_config_file_path(STATELESS_TRANSACTION_VALIDATOR_CONFIG);
-    let fix = false;
-    test_valid_config_body(stateless_transaction_validator_config, file_path, fix);
-}
-
-#[rstest]
-#[ignore]
-/// Fix the config file for test_valid_stateless_transaction_validator_config.
-/// Run with 'cargo test -- --ignored'.
-fn fix_test_valid_stateless_transaction_validator_config(
-    stateless_transaction_validator_config: StatelessTransactionValidatorConfig,
-) {
-    let file_path = get_config_file_path(STATELESS_TRANSACTION_VALIDATOR_CONFIG);
-    let fix = true;
-    test_valid_config_body(stateless_transaction_validator_config, file_path, fix);
+    assert!(stateless_transaction_validator_config.validate().is_ok());
 }
 
 #[rstest]
 /// Read the rpc state reader config file and validate its content.
 fn test_valid_rpc_state_reader_config(rpc_state_reader_config: RpcStateReaderConfig) {
-    let file_path = get_config_file_path(RPC_STATE_READER_CONFIG);
-    let fix = false;
-    test_valid_config_body(rpc_state_reader_config, file_path, fix);
-}
-
-#[rstest]
-#[ignore]
-/// Fix the config file for test_valid_rpc_state_reader_config.
-/// Run with 'cargo test -- --ignored'.
-fn fix_test_valid_rpc_state_reader_config(rpc_state_reader_config: RpcStateReaderConfig) {
-    let file_path = get_config_file_path(RPC_STATE_READER_CONFIG);
-    let fix = true;
-    test_valid_config_body(rpc_state_reader_config, file_path, fix);
+    assert!(rpc_state_reader_config.validate().is_ok());
 }
 
 #[rstest]
 /// Read the gateway config and validate its content.
 fn test_validate_gateway_config(gateway_config: GatewayConfig) {
-    let file_path = get_config_file_path(GATEWAY_CONFIG_FILE);
-    let fix = false;
-    test_valid_config_body(gateway_config, file_path, fix)
-}
-
-#[rstest]
-#[ignore]
-/// Fix the config file for test_valid_gateway_config.
-/// Run with 'cargo test -- --ignored'.
-fn fix_test_validate_gateway_config(gateway_config: GatewayConfig) {
-    let file_path = get_config_file_path(GATEWAY_CONFIG_FILE);
-    let fix = true;
-    test_valid_config_body(gateway_config, file_path, fix)
+    assert!(gateway_config.validate().is_ok());
 }
