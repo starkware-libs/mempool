042d83e5455bec04dbf0ecf678729147e3345cb9
Itay Tsabary <itayt@starkware.co>
2024-05-20 15:45:55 +0300

    feat: wrap mempool as a component with trait, client and server
    commit-id:d5ca218e

--- crates/mempool_types/src/mempool_types.rs
+++ crates/mempool_types/src/mempool_types.rs
@@ -1,3 +1,5 @@
+use async_trait::async_trait;
+use mempool_infra::component_client::ComponentClient;
 use mempool_infra::network_component::NetworkComponent;
 use starknet_api::core::{ContractAddress, Nonce};
 use starknet_api::transaction::{Tip, TransactionHash};
@@ -61,3 +63,50 @@ pub type MempoolNetworkComponent =
     NetworkComponent<MempoolToGatewayMessage, GatewayToMempoolMessage>;
 
 pub type MempoolResult<T> = Result<T, MempoolError>;
+pub type MempoolClientResult<T> = Result<MempoolResult<T>, ClientError>;
+
+// TODO(Tsabary, 1/6/2024): Move communication-related definitions to a separate file.
+pub enum ClientError {
+    UnexpectedResponse,
+}
+
+#[async_trait]
+pub trait MempoolClient: Send + Sync {
+    async fn add_tx(&self, mempool_input: MempoolInput) -> MempoolClientResult<()>;
+    async fn get_txs(&self, n_txs: usize) -> MempoolClientResult<Vec<ThinTransaction>>;
+}
+
+#[derive(Debug)]
+pub enum MempoolRequest {
+    AddTransaction(MempoolInput),
+    GetTransactions(usize),
+}
+
+#[derive(Debug)]
+pub enum MempoolResponse {
+    AddTransaction(MempoolResult<()>),
+    GetTransactions(MempoolResult<Vec<ThinTransaction>>),
+}
+
+type MempoolClientCommunication = ComponentClient<MempoolRequest, MempoolResponse>;
+
+#[async_trait]
+impl MempoolClient for MempoolClientCommunication {
+    async fn add_tx(&self, mempool_input: MempoolInput) -> MempoolClientResult<()> {
+        let request = MempoolRequest::AddTransaction(mempool_input);
+        let res = self.send(request).await;
+        match res {
+            MempoolResponse::AddTransaction(res) => Result::Ok(res),
+            _ => Result::Err(ClientError::UnexpectedResponse),
+        }
+    }
+
+    async fn get_txs(&self, n_txs: usize) -> MempoolClientResult<Vec<ThinTransaction>> {
+        let request = MempoolRequest::GetTransactions(n_txs);
+        let res = self.send(request).await;
+        match res {
+            MempoolResponse::GetTransactions(res) => Result::Ok(res),
+            _ => Result::Err(ClientError::UnexpectedResponse),
+        }
+    }
+}
