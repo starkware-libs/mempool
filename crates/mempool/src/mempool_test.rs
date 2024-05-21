f06e00788f59f9b439a629e49b5329f98e8cc398
Itay Tsabary <itayt@starkware.co>
2024-05-21 12:09:16 +0300

    refactor: move mempool errors to mempool types
    commit-id:35b77aac

--- crates/mempool/src/mempool_test.rs
+++ crates/mempool/src/mempool_test.rs
@@ -9,13 +9,13 @@ use starknet_api::transaction::{
     TransactionHash,
 };
 use starknet_api::{contract_address, patricia_key};
+use starknet_mempool_types::errors::MempoolError;
 use starknet_mempool_types::mempool_types::{
     GatewayToMempoolMessage, MempoolNetworkComponent, MempoolToGatewayMessage,
 };
 use starknet_mempool_types::utils::create_thin_tx_for_testing;
 use tokio::sync::mpsc::channel;
 
-use crate::errors::MempoolError;
 use crate::mempool::{Account, Mempool, MempoolInput};
 use crate::priority_queue::PQTransaction;
 
