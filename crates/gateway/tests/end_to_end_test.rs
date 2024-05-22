521f02241238a9b25b3c82189a3dd11753bbeba8
Itay Tsabary <itayt@starkware.co>
2024-05-22 15:27:06 +0300

    feat: remove deprecated network component from gateway and batcher
    commit-id:4e0d8a62

--- crates/gateway/tests/end_to_end_test.rs
+++ crates/gateway/tests/end_to_end_test.rs
@@ -2,9 +2,8 @@ use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 use std::sync::Arc;
 use std::time::Duration;
 
-use mempool_infra::network_component::CommunicationInterface;
 use rstest::rstest;
-use starknet_api::transaction::{Tip, TransactionHash};
+use starknet_api::transaction::Tip;
 use starknet_gateway::config::{
     GatewayConfig, GatewayNetworkConfig, StatefulTransactionValidatorConfig,
     StatelessTransactionValidatorConfig,
@@ -15,44 +14,13 @@ use starknet_gateway::starknet_api_test_utils::invoke_tx;
 use starknet_gateway::state_reader_test_utils::test_state_reader_factory;
 use starknet_mempool::mempool::{create_mempool_server, Mempool};
 use starknet_mempool_types::mempool_types::{
-    GatewayNetworkComponent, GatewayToMempoolMessage, MempoolClient, MempoolInput,
-    MempoolInterface, MempoolMessageAndResponseSender, MempoolNetworkComponent,
-    MempoolToGatewayMessage,
+    MempoolClient, MempoolInterface, MempoolMessageAndResponseSender,
 };
 use tokio::sync::mpsc::channel;
 use tokio::task;
 use tokio::time::sleep;
 
 const MEMPOOL_INVOCATIONS_QUEUE_SIZE: usize = 32;
-
-#[tokio::test]
-async fn test_send_and_receive() {
-    let (tx_gateway_to_mempool, rx_gateway_to_mempool) = channel::<GatewayToMempoolMessage>(1);
-    let (tx_mempool_to_gateway, rx_mempool_to_gateway) = channel::<MempoolToGatewayMessage>(1);
-
-    let gateway_network =
-        GatewayNetworkComponent::new(tx_gateway_to_mempool, rx_mempool_to_gateway);
-    let mut mempool_network =
-        MempoolNetworkComponent::new(tx_mempool_to_gateway, rx_gateway_to_mempool);
-
-    let tx_hash = TransactionHash::default();
-    let mempool_input = MempoolInput::default();
-    task::spawn(async move {
-        let gateway_to_mempool = GatewayToMempoolMessage::AddTransaction(mempool_input);
-        gateway_network.send(gateway_to_mempool).await.unwrap();
-    })
-    .await
-    .unwrap();
-
-    let mempool_message =
-        task::spawn(async move { mempool_network.recv().await }).await.unwrap().unwrap();
-
-    match mempool_message {
-        GatewayToMempoolMessage::AddTransaction(mempool_input) => {
-            assert_eq!(mempool_input.tx.tx_hash, tx_hash);
-        }
-    }
-}
 
 async fn set_up_gateway(mempool: Box<dyn MempoolInterface>) -> SocketAddr {
     let ip = IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1));
@@ -108,8 +76,6 @@ async fn test_end_to_end() {
     let gateway_client = gateway_client::GatewayClient::new(socket_addr);
     gateway_client.assert_add_tx_success(&external_tx, "INVOKE").await;
 
-    // // Wait for the listener to receive the transactions.
-    // sleep(Duration::from_secs(2)).await;
     let batcher_mempool_client = Box::new(MempoolClient::new(tx_mempool.clone()));
     let mempool_message = batcher_mempool_client.get_txs(2).await.unwrap();
 
--- crates/mempool_types/src/mempool_types.rs
+++ crates/mempool_types/src/mempool_types.rs
@@ -1,10 +1,8 @@
 use async_trait::async_trait;
 use mempool_infra::component_client::ComponentClient;
 use mempool_infra::component_server::MessageAndResponseSender;
-use mempool_infra::network_component::NetworkComponent;
 use starknet_api::core::{ContractAddress, Nonce};
 use starknet_api::transaction::{Tip, TransactionHash};
-use tokio::sync::mpsc::{Receiver, Sender};
 
 use crate::errors::MempoolError;
 
@@ -34,34 +32,6 @@ pub struct MempoolInput {
     pub tx: ThinTransaction,
     pub account: Account,
 }
-
-#[derive(Debug)]
-pub enum GatewayToMempoolMessage {
-    AddTransaction(MempoolInput),
-}
-
-// TODO: Consider using `NetworkComponent` instead of defining the channels here.
-// Currently, facing technical issues when using `NetworkComponent`.
-pub struct BatcherToMempoolChannels {
-    pub rx: Receiver<BatcherToMempoolMessage>,
-    pub tx: Sender<MempoolToBatcherMessage>,
-}
-
-pub enum BatcherToMempoolMessage {
-    GetTransactions(usize),
-}
-pub type MempoolToGatewayMessage = ();
-pub type MempoolToBatcherMessage = Vec<ThinTransaction>;
-
-pub type BatcherMempoolNetworkComponent =
-    NetworkComponent<BatcherToMempoolMessage, MempoolToBatcherMessage>;
-pub type MempoolBatcherNetworkComponent =
-    NetworkComponent<MempoolToBatcherMessage, BatcherToMempoolMessage>;
-
-pub type GatewayNetworkComponent =
-    NetworkComponent<GatewayToMempoolMessage, MempoolToGatewayMessage>;
-pub type MempoolNetworkComponent =
-    NetworkComponent<MempoolToGatewayMessage, GatewayToMempoolMessage>;
 
 pub type MempoolResult<T> = Result<T, MempoolError>;
 
