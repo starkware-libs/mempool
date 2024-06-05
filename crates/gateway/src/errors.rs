c6f83ae342b9790d0d71b8993e3beddbac713815
Arni Hod <arnon@starkware.co>
2024-06-05 16:14:07 +0300

    feat: add declare specific size related stateless tx validator

--- crates/gateway/src/errors.rs
+++ crates/gateway/src/errors.rs
@@ -46,6 +46,33 @@ pub enum StatelessTransactionValidatorError {
         (allowed length: {max_signature_length})."
     )]
     SignatureTooLong { signature_length: usize, max_signature_length: usize },
+    #[error(transparent)]
+    DeclareTransactionError(#[from] DeclareTransactionError),
+    #[error("Error serializing object: {object_name}")]
+    SerializationError { object_name: String },
+}
+
+#[derive(Debug, Error)]
+#[cfg_attr(test, derive(PartialEq))]
+pub enum DeclareTransactionError {
+    #[error(
+        "Declared contract class {bytecode_language} bytecode size is {bytecode_size}. It must be \
+         less then {max_bytecode_size}."
+    )]
+    BytecodeSizeTooLarge {
+        bytecode_language: String,
+        bytecode_size: usize,
+        max_bytecode_size: usize,
+    },
+    #[error(
+        "Declared contract class {bytecode_language} size is {contract_class_object_size}. It \
+         must be less then {max_contract_class_object_size}."
+    )]
+    ContractClassObjectSizeTooLarge {
+        bytecode_language: String,
+        contract_class_object_size: usize,
+        max_contract_class_object_size: usize,
+    },
 }
 
 pub type StatelessTransactionValidatorResult<T> = Result<T, StatelessTransactionValidatorError>;
--- crates/gateway/src/stateless_transaction_validator_test.rs
+++ crates/gateway/src/stateless_transaction_validator_test.rs
@@ -1,17 +1,20 @@
 use assert_matches::assert_matches;
 use rstest::rstest;
-use starknet_api::calldata;
-use starknet_api::external_transaction::ResourceBoundsMapping;
+use starknet_api::external_transaction::{ContractClass, ResourceBoundsMapping};
 use starknet_api::hash::StarkFelt;
 use starknet_api::transaction::{Calldata, Resource, ResourceBounds, TransactionSignature};
+use starknet_api::{calldata, stark_felt};
 
+use crate::config::StatelessTransactionValidatorConfig;
+use crate::declare_tx_args;
+use crate::errors::DeclareTransactionError;
 use crate::starknet_api_test_utils::{
-    create_resource_bounds_mapping, external_tx_for_testing, non_zero_resource_bounds_mapping,
-    zero_resource_bounds_mapping, TransactionType, NON_EMPTY_RESOURCE_BOUNDS,
+    create_resource_bounds_mapping, external_declare_tx, external_tx_for_testing,
+    non_zero_resource_bounds_mapping, zero_resource_bounds_mapping, TransactionType,
+    NON_EMPTY_RESOURCE_BOUNDS,
 };
 use crate::stateless_transaction_validator::{
-    StatelessTransactionValidator, StatelessTransactionValidatorConfig,
-    StatelessTransactionValidatorError,
+    StatelessTransactionValidator, StatelessTransactionValidatorError,
 };
 
 const DEFAULT_VALIDATOR_CONFIG_FOR_TESTING: StatelessTransactionValidatorConfig =
@@ -21,6 +24,8 @@ const DEFAULT_VALIDATOR_CONFIG_FOR_TESTING: StatelessTransactionValidatorConfig 
 
         max_calldata_length: 1,
         max_signature_length: 1,
+        max_bytecode_size: 3,
+        max_raw_class_size: 130,
     };
 
 #[rstest]
@@ -152,4 +157,66 @@ fn test_signature_too_long(
             max_signature_length: 1
         }
     );
+}
+
+#[test]
+fn test_declare_bytecode_size_too_long() {
+    let config_max_bytecode_size = 10;
+    let tx_validator = StatelessTransactionValidator {
+        config: StatelessTransactionValidatorConfig {
+            validate_non_zero_l1_gas_fee: false,
+            validate_non_zero_l2_gas_fee: false,
+            max_bytecode_size: config_max_bytecode_size,
+            ..DEFAULT_VALIDATOR_CONFIG_FOR_TESTING
+        },
+    };
+    let sierra_program = vec![stark_felt!(1_u128); config_max_bytecode_size + 3];
+    let sierra_program_length = sierra_program.len();
+    let contract_class = ContractClass { sierra_program, ..Default::default() };
+    let tx = external_declare_tx(declare_tx_args!(contract_class));
+
+    assert_matches!(
+        tx_validator.validate(&tx).unwrap_err(),
+        StatelessTransactionValidatorError::DeclareTransactionError(
+            DeclareTransactionError::BytecodeSizeTooLarge {
+                bytecode_language,
+                bytecode_size,
+                max_bytecode_size
+            }
+        ) => {
+            assert_eq!(bytecode_language, "Sierra");
+            assert_eq!(bytecode_size, sierra_program_length);
+            assert_eq!(max_bytecode_size, config_max_bytecode_size);
+        }
+    )
+}
+
+#[test]
+fn test_declare_contract_class_size_too_long() {
+    let config_max_raw_class_size = 100;
+    let tx_validator = StatelessTransactionValidator {
+        config: StatelessTransactionValidatorConfig {
+            validate_non_zero_l1_gas_fee: false,
+            validate_non_zero_l2_gas_fee: false,
+            max_raw_class_size: config_max_raw_class_size,
+            ..DEFAULT_VALIDATOR_CONFIG_FOR_TESTING
+        },
+    };
+    let contract_class = ContractClass {
+        sierra_program: vec![stark_felt!(1_u128), stark_felt!(3_u128), stark_felt!(0_u128)],
+        ..Default::default()
+    };
+    let contract_class_len = serde_json::to_string(&contract_class).unwrap().len();
+    let tx = external_declare_tx(declare_tx_args!(contract_class));
+
+    assert_matches!(
+        tx_validator.validate(&tx).unwrap_err(),
+        StatelessTransactionValidatorError::DeclareTransactionError(
+            DeclareTransactionError::ContractClassObjectSizeTooLarge { bytecode_language, contract_class_object_size, max_contract_class_object_size }
+        ) => {
+            assert_eq!(bytecode_language, "Sierra");
+            assert_eq!(contract_class_object_size, contract_class_len);
+            assert_eq!(max_contract_class_object_size, config_max_raw_class_size);
+        }
+    )
 }
