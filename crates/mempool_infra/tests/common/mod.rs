52afb0ea8beb0c965c6e2cab5cda6e966deb2e89
Itay Tsabary <itayt@starkware.co>
2024-06-06 15:07:57 +0300

    chore: move infra test utils from test to common
    commit-id:68fd1fa2

--- crates/mempool_infra/tests/common/mod.rs
+++ crates/mempool_infra/tests/common/mod.rs
@@ -14,3 +14,39 @@ pub(crate) trait ComponentATrait: Send + Sync {
 pub(crate) trait ComponentBTrait: Send + Sync {
     async fn b_get_value(&self) -> ValueB;
 }
+
+pub(crate) struct ComponentA {
+    b: Box<dyn ComponentBTrait>,
+}
+
+#[async_trait]
+impl ComponentATrait for ComponentA {
+    async fn a_get_value(&self) -> ValueA {
+        let b_value = self.b.b_get_value().await;
+        b_value.into()
+    }
+}
+
+impl ComponentA {
+    pub fn new(b: Box<dyn ComponentBTrait>) -> Self {
+        Self { b }
+    }
+}
+
+pub(crate) struct ComponentB {
+    value: ValueB,
+    _a: Box<dyn ComponentATrait>,
+}
+
+#[async_trait]
+impl ComponentBTrait for ComponentB {
+    async fn b_get_value(&self) -> ValueB {
+        self.value
+    }
+}
+
+impl ComponentB {
+    pub fn new(value: ValueB, a: Box<dyn ComponentATrait>) -> Self {
+        Self { value, _a: a }
+    }
+}
--- crates/mempool_infra/tests/component_server_client_test.rs
+++ crates/mempool_infra/tests/component_server_client_test.rs
@@ -10,26 +10,10 @@ use starknet_mempool_infra::component_server::ComponentServer;
 use tokio::sync::mpsc::{channel, Sender};
 use tokio::task;
 
+use crate::common::{ComponentA, ComponentB};
+
 type ValueA = u32;
 type ValueB = u8;
-
-struct ComponentA {
-    b: Box<dyn ComponentBTrait>,
-}
-
-#[async_trait]
-impl ComponentATrait for ComponentA {
-    async fn a_get_value(&self) -> ValueA {
-        let b_value = self.b.b_get_value().await;
-        b_value.into()
-    }
-}
-
-impl ComponentA {
-    fn new(b: Box<dyn ComponentBTrait>) -> Self {
-        Self { b }
-    }
-}
 
 // TODO(Tsabary): send messages from component b to component a.
 
@@ -57,24 +41,6 @@ impl ComponentRequestHandler<ComponentARequest, ComponentAResponse> for Componen
         match request {
             ComponentARequest::AGetValue => ComponentAResponse::Value(self.a_get_value().await),
         }
-    }
-}
-
-struct ComponentB {
-    value: ValueB,
-    _a: Box<dyn ComponentATrait>,
-}
-
-#[async_trait]
-impl ComponentBTrait for ComponentB {
-    async fn b_get_value(&self) -> ValueB {
-        self.value
-    }
-}
-
-impl ComponentB {
-    fn new(value: ValueB, a: Box<dyn ComponentATrait>) -> Self {
-        Self { value, _a: a }
     }
 }
 
