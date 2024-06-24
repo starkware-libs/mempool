4948849e2c46e5ce9e11c0e8679c53f3558e52ed
Uriel Korach <uriel@starkware.co>
2024-06-24 17:52:31 +0300

    test: generic client/server for component communication using hyper

--- crates/mempool_infra/src/component_client.rs
+++ crates/mempool_infra/src/component_client.rs
@@ -1,9 +1,22 @@
+use std::net::IpAddr;
+
+use bincode::serialize;
+use hyper::{Body, Client, Request as HyperRequest, Uri};
+use serde::{Deserialize, Serialize};
 use thiserror::Error;
 use tokio::sync::mpsc::{channel, Sender};
 
 use crate::component_definitions::ComponentRequestAndResponseSender;
 
-pub struct ComponentClient<Request, Response>
+#[derive(Debug, Error)]
+pub enum ClientError {
+    #[error("Got an unexpected response type.")]
+    UnexpectedResponse,
+}
+
+pub type ClientResult<Response> = Result<Response, ClientError>;
+
+pub struct ComponentClientChannel<Request, Response>
 where
     Request: Send + Sync,
     Response: Send + Sync,
@@ -11,7 +24,7 @@ where
     tx: Sender<ComponentRequestAndResponseSender<Request, Response>>,
 }
 
-impl<Request, Response> ComponentClient<Request, Response>
+impl<Request, Response> ComponentClientChannel<Request, Response>
 where
     Request: Send + Sync,
     Response: Send + Sync,
@@ -22,18 +35,18 @@ where
 
     // TODO(Tsabary, 1/5/2024): Consider implementation for messages without expected responses.
 
-    pub async fn send(&self, request: Request) -> Response {
+    pub async fn send(&self, request: Request) -> ClientResult<Response> {
         let (res_tx, mut res_rx) = channel::<Response>(1);
         let request_and_res_tx = ComponentRequestAndResponseSender { request, tx: res_tx };
         self.tx.send(request_and_res_tx).await.expect("Outbound connection should be open.");
 
-        res_rx.recv().await.expect("Inbound connection should be open.")
+        Ok(res_rx.recv().await.expect("Inbound connection should be open."))
     }
 }
 
 // Can't derive because derive forces the generics to also be `Clone`, which we prefer not to do
 // since it'll require transactions to be cloneable.
-impl<Request, Response> Clone for ComponentClient<Request, Response>
+impl<Request, Response> Clone for ComponentClientChannel<Request, Response>
 where
     Request: Send + Sync,
     Response: Send + Sync,
@@ -43,8 +56,42 @@ where
     }
 }
 
-#[derive(Debug, Error)]
-pub enum ClientError {
-    #[error("Got an unexpected response type.")]
-    UnexpectedResponse,
+pub struct ComponentClientHttp<Component> {
+    uri: Uri,
+    _component: std::marker::PhantomData<Component>,
+}
+
+impl<Component> ComponentClientHttp<Component> {
+    pub fn new(ip_address: IpAddr, port: u16) -> Self {
+        let uri = match ip_address {
+            IpAddr::V4(ip_address) => format!("http://{}:{}/", ip_address, port).parse().unwrap(),
+            IpAddr::V6(ip_address) => format!("http://[{}]:{}/", ip_address, port).parse().unwrap(),
+        };
+        Self { uri, _component: Default::default() }
+    }
+
+    pub async fn send<Request, Response>(
+        &self,
+        component_request: Request,
+    ) -> ClientResult<Response>
+    where
+        Request: Serialize,
+        Response: for<'a> Deserialize<'a>,
+    {
+        let http_request = HyperRequest::post(self.uri.clone())
+            .header("Content-Type", "application/octet-stream")
+            .body(Body::from(
+                serialize(&component_request).expect("Request serialization should succeed"),
+            ))
+            .expect("Request builidng should succeed");
+
+        // TODO(uriel): Add configuration to control number of retries
+        let http_response =
+            Client::new().request(http_request).await.expect("Could not connect to server");
+        let body_bytes = hyper::body::to_bytes(http_response.into_body())
+            .await
+            .expect("Could not get response from server");
+
+        Ok(bincode::deserialize(&body_bytes).expect("Response deserialization should succeed"))
+    }
 }
--- crates/mempool_infra/tests/common/mod.rs
+++ crates/mempool_infra/tests/common/mod.rs
@@ -1,52 +1,90 @@
 use async_trait::async_trait;
+use serde::{Deserialize, Serialize};
+use starknet_mempool_infra::component_client::ClientResult;
+use starknet_mempool_infra::component_server::ComponentRequestHandler;
+
+// TODO(Tsabary): add more messages / functions to the components.
 
 pub(crate) type ValueA = u32;
 pub(crate) type ValueB = u8;
 
-// TODO(Tsabary): add more messages / functions to the components.
+pub type ResultA = ClientResult<ValueA>;
+pub type ResultB = ClientResult<ValueB>;
 
-#[async_trait]
-pub(crate) trait ComponentATrait: Send + Sync {
-    async fn a_get_value(&self) -> ValueA;
+#[derive(Serialize, Deserialize, Debug)]
+pub enum ComponentARequest {
+    AGetValue,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub enum ComponentAResponse {
+    Value(ValueA),
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub enum ComponentBRequest {
+    BGetValue,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub enum ComponentBResponse {
+    Value(ValueB),
 }
 
 #[async_trait]
-pub(crate) trait ComponentBTrait: Send + Sync {
-    async fn b_get_value(&self) -> ValueB;
+#[allow(dead_code)]
+pub(crate) trait ComponentAClientTrait: Send + Sync {
+    async fn a_get_value(&self) -> ResultA;
+}
+
+#[async_trait]
+pub(crate) trait ComponentBClientTrait: Send + Sync {
+    async fn b_get_value(&self) -> ResultB;
 }
 
 pub(crate) struct ComponentA {
-    b: Box<dyn ComponentBTrait>,
-}
-
-#[async_trait]
-impl ComponentATrait for ComponentA {
-    async fn a_get_value(&self) -> ValueA {
-        let b_value = self.b.b_get_value().await;
-        b_value.into()
-    }
+    b: Box<dyn ComponentBClientTrait>,
 }
 
 impl ComponentA {
-    pub fn new(b: Box<dyn ComponentBTrait>) -> Self {
+    pub fn new(b: Box<dyn ComponentBClientTrait>) -> Self {
         Self { b }
+    }
+
+    pub async fn a_get_value(&self) -> ValueA {
+        self.b.b_get_value().await.unwrap().into()
+    }
+}
+
+#[async_trait]
+impl ComponentRequestHandler<ComponentARequest, ComponentAResponse> for ComponentA {
+    async fn handle_request(&mut self, request: ComponentARequest) -> ComponentAResponse {
+        match request {
+            ComponentARequest::AGetValue => ComponentAResponse::Value(self.a_get_value().await),
+        }
     }
 }
 
 pub(crate) struct ComponentB {
     value: ValueB,
-    _a: Box<dyn ComponentATrait>,
+    _a: Box<dyn ComponentAClientTrait>,
 }
 
-#[async_trait]
-impl ComponentBTrait for ComponentB {
-    async fn b_get_value(&self) -> ValueB {
+impl ComponentB {
+    pub fn new(value: ValueB, a: Box<dyn ComponentAClientTrait>) -> Self {
+        Self { value, _a: a }
+    }
+
+    pub fn b_get_value(&self) -> ValueB {
         self.value
     }
 }
 
-impl ComponentB {
-    pub fn new(value: ValueB, a: Box<dyn ComponentATrait>) -> Self {
-        Self { value, _a: a }
+#[async_trait]
+impl ComponentRequestHandler<ComponentBRequest, ComponentBResponse> for ComponentB {
+    async fn handle_request(&mut self, request: ComponentBRequest) -> ComponentBResponse {
+        match request {
+            ComponentBRequest::BGetValue => ComponentBResponse::Value(self.b_get_value()),
+        }
     }
 }
