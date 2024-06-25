e2adcf5ea77dd7512f878d809c9cc425a43d0f55
Yael Doweck <yael@starkware.co>
2024-06-25 08:39:16 +0300

    test: add cairo0 to integration test

--- crates/tests-integration/tests/end_to_end_test.rs
+++ crates/tests-integration/tests/end_to_end_test.rs
@@ -6,10 +6,15 @@ use starknet_mempool_integration_tests::integration_test_setup::IntegrationTestS
 async fn test_end_to_end() {
     let mut mock_running_system = IntegrationTestSetup::new().await;
 
-    let expected_tx_hash =
-        mock_running_system.assert_add_tx_success(&invoke_tx(CairoVersion::Cairo1)).await;
-
-    let mempool_txs = mock_running_system.get_txs(2).await;
-    assert_eq!(mempool_txs.len(), 1);
-    assert_eq!(mempool_txs[0].tx_hash, expected_tx_hash);
+    let mut expected_tx_hash = Vec::new();
+    expected_tx_hash
+        .push(mock_running_system.assert_add_tx_success(&invoke_tx(CairoVersion::Cairo0)).await);
+    expected_tx_hash
+        .push(mock_running_system.assert_add_tx_success(&invoke_tx(CairoVersion::Cairo1)).await);
+ 
+    let mempool_txs = mock_running_system.get_txs(4).await;
+    assert_eq!(mempool_txs.len(), 2);
+    expected_tx_hash.iter().for_each(|tx_hash| {
+        assert!(mempool_txs.iter().any(|tx| tx.tx_hash == *tx_hash), "Did not get tx_hash: {:?} from mempool", tx_hash);
+    });
 }
