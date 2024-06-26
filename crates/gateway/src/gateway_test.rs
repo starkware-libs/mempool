77f752cc09afebb380e6aed487aa5fca05c89ec4
Lev Roitman <lev@starkware.co>
2024-06-26 18:09:23 +0300

    feat: adding empty server and refactoring ComponentServer
    commit-id:689882a6

--- crates/gateway/src/gateway_test.rs
+++ crates/gateway/src/gateway_test.rs
@@ -15,6 +15,7 @@ use starknet_api::rpc_transaction::{RPCDeclareTransaction, RPCTransaction};
 use starknet_api::transaction::TransactionHash;
 use starknet_mempool::communication::create_mempool_server;
 use starknet_mempool::mempool::Mempool;
+use starknet_mempool_infra::component_server::ComponentServerStarter;
 use starknet_mempool_types::communication::{MempoolClientImpl, MempoolRequestAndResponseSender};
 use starknet_sierra_compile::errors::CompilationUtilError;
 use tokio::sync::mpsc::channel;
