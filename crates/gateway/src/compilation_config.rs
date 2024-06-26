491e06ff4cc56e6a9f3a0be091b3bed0fd0fbd70
Arni Hod <arnon@starkware.co>
2024-06-26 11:43:46 +0300

    feat: post compilation size limit validation

--- crates/gateway/src/compilation_config.rs
+++ crates/gateway/src/compilation_config.rs
@@ -1,15 +1,37 @@
 use std::collections::BTreeMap;
 
-use papyrus_config::dumping::SerializeConfig;
-use papyrus_config::{ParamPath, SerializedParam};
+use papyrus_config::dumping::{ser_param, SerializeConfig};
+use papyrus_config::{ParamPath, ParamPrivacyInput, SerializedParam};
 use serde::{Deserialize, Serialize};
 use validator::Validate;
 
-#[derive(Clone, Debug, Default, Serialize, Deserialize, Validate, PartialEq)]
-pub struct GatewayCompilerConfig {}
+#[derive(Clone, Debug, Serialize, Deserialize, Validate, PartialEq)]
+pub struct GatewayCompilerConfig {
+    pub max_bytecode_size: usize,
+    pub max_raw_class_size: usize,
+}
+
+impl Default for GatewayCompilerConfig {
+    fn default() -> Self {
+        Self { max_bytecode_size: 81920, max_raw_class_size: 4089446 }
+    }
+}
 
 impl SerializeConfig for GatewayCompilerConfig {
     fn dump(&self) -> BTreeMap<ParamPath, SerializedParam> {
-        BTreeMap::new()
+        BTreeMap::from_iter([
+            ser_param(
+                "max_bytecode_size",
+                &self.max_bytecode_size,
+                "Limitation of contract bytecode size",
+                ParamPrivacyInput::Public,
+            ),
+            ser_param(
+                "max_raw_class_size",
+                &self.max_raw_class_size,
+                "Limitation of contract class object size",
+                ParamPrivacyInput::Public,
+            ),
+        ])
     }
 }
