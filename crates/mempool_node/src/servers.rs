ba88c86353366bd52a2d8089bdcb47c6423c12ba
Lev Roitman <lev@starkware.co>
2024-06-26 18:59:35 +0300

    feat: add functionality to create all servers
    commit-id:0f64f96b

---
+++ crates/mempool_node/src/servers.rs
@@ -0,0 +1,41 @@
+// use std::future::pending;
+// use std::pin::Pin;
+
+// use futures::{Future, FutureExt};
+use starknet_gateway::gateway::create_gateway_server;
+use starknet_mempool::communication::create_mempool_server;
+use starknet_mempool_infra::component_server::ComponentServerStarter;
+
+use crate::communication::MempoolNodeCommunication;
+use crate::components::Components;
+use crate::config::MempoolNodeConfig;
+
+pub struct Servers {
+    pub gateway: Option<Box<dyn ComponentServerStarter>>,
+    pub mempool: Option<Box<dyn ComponentServerStarter>>,
+}
+
+pub fn create_servers(
+    config: &MempoolNodeConfig,
+    mut channels: MempoolNodeCommunication,
+    components: Components,
+) -> Servers {
+    let mut servers = Servers { gateway: None, mempool: None };
+
+    if config.components.gateway_component.execute {
+        servers.gateway = Some(Box::new(create_gateway_server(
+            components.gateway.expect("Gateway component is not initialized."),
+        )));
+    }
+
+    if config.components.mempool_component.execute {
+        servers.mempool = Some(Box::new(create_mempool_server(
+            components.mempool.expect("Mempool component is not initialized."),
+            channels.take_mempool_rx(),
+        )));
+    }
+
+    servers
+}
+
+// TODO (Lev): Implement the run server components function.
