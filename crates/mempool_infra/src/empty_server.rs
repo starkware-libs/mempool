197644c76a0761616bd0f4c77630c49b6b6f44a4
Lev Roitman <lev@starkware.co>
2024-06-26 18:09:23 +0300

    feat: adding empty server and refactoring ComponentServer
    commit-id:689882a6

---
+++ crates/mempool_infra/src/empty_server.rs
@@ -0,0 +1,33 @@
+use async_trait::async_trait;
+
+// use tokio::sync::mpsc::channel;
+
+// use crate::component_definitions::{ComponentRequestAndResponseSender,
+// ComponentRequestHandler}; use crate::component_runner::{ComponentRunner,
+// ComponentStartError};
+use crate::component_runner::ComponentRunner;
+use crate::component_server::CommunicationServer;
+
+pub struct EmptyServer<T: ComponentRunner + Send> {
+    component: T,
+}
+
+impl<T: ComponentRunner + Send + Sync> EmptyServer<T> {
+    pub fn new(component: T) -> Self {
+        Self { component }
+    }
+}
+
+#[async_trait]
+impl<T: ComponentRunner + Send + Sync> CommunicationServer for EmptyServer<T> {
+    async fn start(&mut self) {
+        match self.component.start().await {
+            Ok(_) => println!("ComponentServer::start() completed."),
+            Err(err) => println!("ComponentServer::start() failed: {:?}", err),
+        }
+    }
+}
+
+pub fn create_empty_server<T: ComponentRunner + Send + Sync>(component: T) -> EmptyServer<T> {
+    EmptyServer::new(component)
+}
