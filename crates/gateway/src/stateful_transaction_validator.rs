67f7a22431899286a5ad398d8fa11f2b5bc4a28e
Yael Doweck <yael@starkware.co>
2024-07-02 11:03:12 +0300

    feat: skip validation of an invoke transaction after deploy account

--- crates/gateway/src/stateful_transaction_validator.rs
+++ crates/gateway/src/stateful_transaction_validator.rs
@@ -5,13 +5,15 @@ use blockifier::context::BlockContext;
 use blockifier::execution::contract_class::ClassInfo;
 use blockifier::state::cached_state::CachedState;
 use blockifier::versioned_constants::VersionedConstants;
-use starknet_api::rpc_transaction::RPCTransaction;
+use starknet_api::core::Nonce;
+use starknet_api::hash::StarkFelt;
+use starknet_api::rpc_transaction::{RPCInvokeTransaction, RPCTransaction};
 use starknet_api::transaction::TransactionHash;
 
 use crate::config::StatefulTransactionValidatorConfig;
 use crate::errors::{StatefulTransactionValidatorError, StatefulTransactionValidatorResult};
 use crate::state_reader::{MempoolStateReader, StateReaderFactory};
-use crate::utils::{external_tx_to_account_tx, get_tx_hash};
+use crate::utils::{external_tx_to_account_tx, get_sender_address, get_tx_hash};
 
 #[cfg(test)]
 #[path = "stateful_transaction_validator_test.rs"]
@@ -27,12 +29,12 @@ impl StatefulTransactionValidator {
         state_reader_factory: &dyn StateReaderFactory,
         external_tx: &RPCTransaction,
         optional_class_info: Option<ClassInfo>,
-        deploy_account_tx_hash: Option<TransactionHash>,
     ) -> StatefulTransactionValidatorResult<TransactionHash> {
         // TODO(yael 6/5/2024): consider storing the block_info as part of the
         // StatefulTransactionValidator and update it only once a new block is created.
         let latest_block_info = get_latest_block_info(state_reader_factory)?;
-        let state_reader = state_reader_factory.get_state_reader(latest_block_info.block_number);
+        let latest_block_number = latest_block_info.block_number;
+        let state_reader = state_reader_factory.get_state_reader(latest_block_number);
         let state = CachedState::new(state_reader);
         let versioned_constants = VersionedConstants::latest_constants_with_overrides(
             self.config.validate_max_n_steps,
@@ -52,20 +54,44 @@ impl StatefulTransactionValidator {
             &versioned_constants,
         );
 
-        let mut validator = BlockifierStatefulValidator::create(
-            state,
-            block_context,
-            self.config.max_nonce_for_validation_skip,
-            BouncerConfig::max(),
-        );
+        let mut validator =
+            BlockifierStatefulValidator::create(state, block_context, BouncerConfig::max());
         let account_tx = external_tx_to_account_tx(
             external_tx,
             optional_class_info,
             &self.config.chain_info.chain_id,
         )?;
         let tx_hash = get_tx_hash(&account_tx);
-        validator.perform_validations(account_tx, deploy_account_tx_hash)?;
+
+        let account_nonce = validator.get_nonce(get_sender_address(external_tx))?;
+        let skip_validate = skip_stateful_validations(
+            external_tx,
+            account_nonce,
+            self.config.max_nonce_for_validation_skip,
+        )?;
+        validator.perform_validations(account_tx, skip_validate)?;
         Ok(tx_hash)
+    }
+}
+
+// Check if validation of an invoke transaction should be skipped due to deploy_account not being
+// proccessed yet. This feature is used to improve UX for users sending deploy_account + invoke at
+// once.
+fn skip_stateful_validations(
+    tx: &RPCTransaction,
+    account_nonce: Nonce,
+    max_nonce_for_validation_skip: Nonce,
+) -> StatefulTransactionValidatorResult<bool> {
+    match tx {
+        RPCTransaction::Invoke(RPCInvokeTransaction::V3(tx)) => {
+            // check if the transaction nonce is bigger than 1, meaning it is post
+            // deploy_account, and the account nonce is zero, meaning the account was not deployed
+            // yet. The mempool also verifies that the deploy_account transaction exists.
+            Ok(tx.nonce >= Nonce(StarkFelt::ONE)
+                && tx.nonce <= max_nonce_for_validation_skip
+                && account_nonce == Nonce(StarkFelt::ZERO))
+        }
+        RPCTransaction::DeployAccount(_) | RPCTransaction::Declare(_) => Ok(false),
     }
 }
 
