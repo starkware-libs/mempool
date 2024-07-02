00f80014f4d96cb4a03e78429574560cce05994e
Yael Doweck <yael@starkware.co>
2024-07-02 11:03:12 +0300

    feat: skip validation of an invoke transaction after deploy account

--- crates/gateway/src/stateful_transaction_validator_test.rs
+++ crates/gateway/src/stateful_transaction_validator_test.rs
@@ -1,15 +1,21 @@
+use assert_matches::assert_matches;
 use blockifier::blockifier::stateful_validator::StatefulValidatorError;
 use blockifier::context::BlockContext;
+use blockifier::test_utils::dict_state_reader::DictStateReader;
 use blockifier::test_utils::CairoVersion;
-use blockifier::transaction::errors::{TransactionFeeError, TransactionPreValidationError};
+use blockifier::transaction::errors::TransactionPreValidationError;
+use blockifier::transaction::test_utils::block_context;
 use num_bigint::BigUint;
-use rstest::rstest;
+use rstest::{fixture, rstest};
+use starknet_api::core::{ContractAddress, Nonce};
 use starknet_api::felt;
 use starknet_api::rpc_transaction::RPCTransaction;
 use starknet_api::transaction::TransactionHash;
+use starknet_types_core::felt::Felt;
+use test_utils::invoke_tx_args;
 use test_utils::starknet_api_test_utils::{
-    declare_tx, deploy_account_tx, invoke_tx, VALID_L1_GAS_MAX_AMOUNT,
-    VALID_L1_GAS_MAX_PRICE_PER_UNIT,
+    declare_tx, deploy_account_tx, external_invoke_tx, invoke_tx, TEST_SENDER_ADDRESS,
+    VALID_L1_GAS_MAX_AMOUNT, VALID_L1_GAS_MAX_PRICE_PER_UNIT,
 };
 
 use crate::compilation::compile_contract_class;
@@ -17,9 +23,22 @@ use crate::config::StatefulTransactionValidatorConfig;
 use crate::errors::{StatefulTransactionValidatorError, StatefulTransactionValidatorResult};
 use crate::state_reader_test_utils::{
     local_test_state_reader_factory, local_test_state_reader_factory_for_deploy_account,
-    TestStateReaderFactory,
+    TestStateReader, TestStateReaderFactory,
 };
 use crate::stateful_transaction_validator::StatefulTransactionValidator;
+use blockifier::transaction::errors::TransactionFeeError;
+
+#[fixture]
+fn stateful_validator(block_context: BlockContext) -> StatefulTransactionValidator {
+    StatefulTransactionValidator {
+        config: StatefulTransactionValidatorConfig {
+            max_nonce_for_validation_skip: Default::default(),
+            validate_max_n_steps: block_context.versioned_constants().validate_max_n_steps,
+            max_recursion_depth: block_context.versioned_constants().max_recursion_depth,
+            chain_info: block_context.chain_info().clone().into(),
+        },
+    }
+}
 
 #[rstest]
 #[case::valid_invoke_tx_cairo1(
@@ -69,26 +88,76 @@ fn test_stateful_tx_validator(
     #[case] external_tx: RPCTransaction,
     #[case] state_reader_factory: TestStateReaderFactory,
     #[case] expected_result: StatefulTransactionValidatorResult<TransactionHash>,
+    stateful_validator: StatefulTransactionValidator,
 ) {
-    let block_context = &BlockContext::create_for_testing();
-    let stateful_validator = StatefulTransactionValidator {
-        config: StatefulTransactionValidatorConfig {
-            max_nonce_for_validation_skip: Default::default(),
-            validate_max_n_steps: block_context.versioned_constants().validate_max_n_steps,
-            max_recursion_depth: block_context.versioned_constants().max_recursion_depth,
-            chain_info: block_context.chain_info().clone().into(),
-        },
-    };
     let optional_class_info = match &external_tx {
         RPCTransaction::Declare(declare_tx) => Some(compile_contract_class(declare_tx).unwrap()),
         _ => None,
     };
 
-    let result = stateful_validator.run_validate(
-        &state_reader_factory,
-        &external_tx,
-        optional_class_info,
-        None,
-    );
+    let result =
+        stateful_validator.run_validate(&state_reader_factory, &external_tx, optional_class_info);
     assert_eq!(format!("{:?}", result), format!("{:?}", expected_result));
+}
+
+#[rstest]
+#[case::should_skip_validation(
+    external_invoke_tx(invoke_tx_args!{nonce: Nonce(Felt::ONE)}),
+    empty_state_reader_factory(),
+    true
+)]
+#[case::should_not_skip_validation_nonce_over_max_nonce_for_skip(
+    external_invoke_tx(invoke_tx_args!{nonce: Nonce(Felt::TWO)}),
+    empty_state_reader_factory(),
+    false
+)]
+#[case::should_not_skip_validation_non_invoke(
+    deploy_account_tx(),
+    empty_state_reader_factory(),
+    false
+)]
+#[case::should_not_skip_validation_account_nonce_1(
+    external_invoke_tx(invoke_tx_args!{sender_address: ContractAddress::from(TEST_SENDER_ADDRESS), nonce: Nonce(Felt::ONE)}),
+    state_reader_factory_account_nonce_1(ContractAddress::from(TEST_SENDER_ADDRESS)),
+    false
+)]
+fn test_skip_stateful_validation(
+    #[case] external_tx: RPCTransaction,
+    #[case] state_reader_factory: TestStateReaderFactory,
+    #[case] should_pass_validation: bool,
+    mut stateful_validator: StatefulTransactionValidator,
+) {
+    // Enable the validator skip validation for nonce = 1.
+    stateful_validator.config.max_nonce_for_validation_skip = Nonce(Felt::ONE);
+
+    let result = stateful_validator.run_validate(&state_reader_factory, &external_tx, None);
+    if should_pass_validation {
+        assert_matches!(result, Ok(_));
+    } else {
+        // To be sure that the validations were actually skipped, we check that the error came from
+        // the blockifier stateful validations, and not from the pre validations since those are
+        // executed also when skip_validate is true.
+        assert_matches!(result, Err(StatefulTransactionValidatorError::StatefulValidatorError(err)) 
+            if !matches!(err, StatefulValidatorError::TransactionPreValidationError(_)));
+    }
+}
+
+fn empty_state_reader_factory() -> TestStateReaderFactory {
+    let block_context = BlockContext::create_for_testing();
+    TestStateReaderFactory {
+        state_reader: TestStateReader {
+            blockifier_state_reader: DictStateReader::default(),
+            block_info: block_context.block_info().clone(),
+        },
+    }
+}
+
+fn state_reader_factory_account_nonce_1(sender_address: ContractAddress) -> TestStateReaderFactory {
+    let mut state_reader_factory = empty_state_reader_factory();
+    state_reader_factory
+        .state_reader
+        .blockifier_state_reader
+        .address_to_nonce
+        .insert(sender_address, Nonce(Felt::ONE));
+    state_reader_factory
 }
