acc2304eb41adc161b6c02bd533733f6bca251a8
Arni Hod <arnon@starkware.co>
2024-07-07 12:15:38 +0300

    chore: create struct for gateway compiler

--- crates/gateway/src/gateway.rs
+++ crates/gateway/src/gateway.rs
@@ -13,7 +13,7 @@ use starknet_mempool_types::communication::SharedMempoolClient;
 use starknet_mempool_types::mempool_types::{Account, MempoolInput};
 use tracing::info;
 
-use crate::compilation::compile_contract_class;
+use crate::compilation::GatewayCompiler;
 use crate::config::{GatewayConfig, GatewayNetworkConfig, RpcStateReaderConfig};
 use crate::errors::{GatewayError, GatewayResult, GatewayRunError};
 use crate::rpc_state_reader::RpcStateReaderFactory;
@@ -120,7 +120,10 @@ fn process_tx(
 
     // Compile Sierra to Casm.
     let optional_class_info = match &tx {
-        RPCTransaction::Declare(declare_tx) => Some(compile_contract_class(declare_tx)?),
+        RPCTransaction::Declare(declare_tx) => {
+            let gateway_compiler = GatewayCompiler { config: Default::default() };
+            Some(gateway_compiler.compile_contract_class(declare_tx)?)
+        }
         _ => None,
     };
 
