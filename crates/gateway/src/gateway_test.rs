10f9f75fce77b009836ec17946d6ebb479bdf183
Yair Bakalchuk <yair@starkware.co>
2024-07-07 15:30:35 +0300

    test(gateway): use mock mempool client

--- crates/gateway/src/gateway_test.rs
+++ crates/gateway/src/gateway_test.rs
@@ -6,33 +6,17 @@ use axum::http::StatusCode;
 use axum::response::{IntoResponse, Response};
 use blockifier::context::ChainInfo;
 use blockifier::test_utils::CairoVersion;
-use rstest::{fixture, rstest};
 use starknet_api::rpc_transaction::RPCTransaction;
 use starknet_api::transaction::TransactionHash;
-use starknet_mempool::communication::create_mempool_server;
-use starknet_mempool::mempool::Mempool;
-use starknet_mempool_infra::component_server::ComponentServerStarter;
-use starknet_mempool_types::communication::{MempoolClientImpl, MempoolRequestAndResponseSender};
-use test_utils::starknet_api_test_utils::{declare_tx, deploy_account_tx, invoke_tx};
-use tokio::sync::mpsc::channel;
-use tokio::task;
+use starknet_mempool_types::communication::MockMempoolClient;
+use test_utils::starknet_api_test_utils::invoke_tx;
 
 use crate::config::{StatefulTransactionValidatorConfig, StatelessTransactionValidatorConfig};
 use crate::gateway::{add_tx, compile_contract_class, AppState, SharedMempoolClient};
-use crate::state_reader_test_utils::{
-    local_test_state_reader_factory, local_test_state_reader_factory_for_deploy_account,
-    TestStateReaderFactory,
-};
+use crate::state_reader_test_utils::{local_test_state_reader_factory, TestStateReaderFactory};
 use crate::stateful_transaction_validator::StatefulTransactionValidator;
 use crate::stateless_transaction_validator::StatelessTransactionValidator;
 use crate::utils::{external_tx_to_account_tx, get_tx_hash};
-
-const MEMPOOL_INVOCATIONS_QUEUE_SIZE: usize = 32;
-
-#[fixture]
-fn mempool() -> Mempool {
-    Mempool::empty()
-}
 
 pub fn app_state(
     mempool_client: SharedMempoolClient,
@@ -57,43 +41,14 @@ pub fn app_state(
     }
 }
 
-// TODO(Ayelet): add test cases for declare.
 #[tokio::test]
-#[rstest]
-#[case::valid_invoke_tx_cairo1(
-    invoke_tx(CairoVersion::Cairo1),
-    local_test_state_reader_factory(CairoVersion::Cairo1, false)
-)]
-#[case::valid_invoke_tx_cairo0(
-    invoke_tx(CairoVersion::Cairo0),
-    local_test_state_reader_factory(CairoVersion::Cairo0, false)
-)]
-#[case::valid_deploy_account_tx(
-    deploy_account_tx(),
-    local_test_state_reader_factory_for_deploy_account(&tx)
-)]
-#[case::valid_declare_tx(
-    declare_tx(),
-    local_test_state_reader_factory(CairoVersion::Cairo1, false)
-)]
-async fn test_add_tx(
-    #[case] tx: RPCTransaction,
-    #[case] state_reader_factory: TestStateReaderFactory,
-    mempool: Mempool,
-) {
-    // TODO(Tsabary): wrap creation of channels in dedicated functions, take channel capacity from
-    // config.
-    let (tx_mempool, rx_mempool) =
-        channel::<MempoolRequestAndResponseSender>(MEMPOOL_INVOCATIONS_QUEUE_SIZE);
-    let mut mempool_server = create_mempool_server(mempool, rx_mempool);
-    task::spawn(async move {
-        mempool_server.start().await;
-    });
+async fn test_add_tx() {
+    let mut mock_mempool_client = MockMempoolClient::new();
+    mock_mempool_client.expect_add_tx().once().return_once(|_| Ok(()));
+    let state_reader_factory = local_test_state_reader_factory(CairoVersion::Cairo1, false);
+    let app_state = app_state(Arc::new(mock_mempool_client), state_reader_factory);
 
-    let mempool_client = Arc::new(MempoolClientImpl::new(tx_mempool));
-
-    let app_state = app_state(mempool_client, state_reader_factory);
-
+    let tx = invoke_tx(CairoVersion::Cairo1);
     let tx_hash = calculate_hash(&tx);
     let response = add_tx(State(app_state), tx.into()).await.into_response();
 
