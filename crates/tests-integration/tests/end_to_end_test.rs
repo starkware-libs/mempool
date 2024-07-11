2c1b04c7abb3c199b8342bafb5a4a2e4fdd51818
Gilad Chase <gilad@starkware.com>
2024-07-11 18:17:47 +0300

    fix: fix e2e test by using tx generator
    - Make e2e use tx generator txs, which track nonces.
    - As a result, the test now reflects the way the mempool behaves at the
      moment, and so only nonce-0 txs are returned from get_txs.
    - Once get_txs begin replenishing the queue when the queue gets
      exhausted, this test should be fixed as per the comment below.

--- crates/tests-integration/tests/end_to_end_test.rs
+++ crates/tests-integration/tests/end_to_end_test.rs
@@ -1,26 +1,47 @@
 use blockifier::test_utils::contracts::FeatureContract;
 use blockifier::test_utils::CairoVersion;
-use mempool_test_utils::starknet_api_test_utils::{deploy_account_tx, invoke_tx};
 use starknet_api::transaction::TransactionHash;
-use starknet_mempool_integration_tests::integration_test_setup::IntegrationTestSetup;
+use starknet_mempool_integration_tests::integration_test_utils::setup_with_tx_generation;
 
 #[tokio::test]
 async fn test_end_to_end() {
-    let mock_running_system = IntegrationTestSetup::new_for_account_contracts([
-        FeatureContract::AccountWithoutValidations(CairoVersion::Cairo0),
+    let accounts = [
         FeatureContract::AccountWithoutValidations(CairoVersion::Cairo1),
-    ])
-    .await;
-
+        FeatureContract::AccountWithoutValidations(CairoVersion::Cairo0),
+    ];
+    let (mock_running_system, mut tx_generator) = setup_with_tx_generation(&accounts).await;
     let mut expected_tx_hashes = Vec::new();
-    expected_tx_hashes
-        .push(mock_running_system.assert_add_tx_success(&invoke_tx(CairoVersion::Cairo0)).await);
-    expected_tx_hashes
-        .push(mock_running_system.assert_add_tx_success(&invoke_tx(CairoVersion::Cairo1)).await);
-    expected_tx_hashes.push(mock_running_system.assert_add_tx_success(&deploy_account_tx()).await);
+
+    let account0_deploy_nonce0 = &tx_generator.account_with_id(0).generate_default_deploy_account();
+    expected_tx_hashes.push(
+        mock_running_system
+            .assert_add_tx_success(account0_deploy_nonce0).await
+    );
+
+    let account0_invoke_nonce1 = tx_generator.account_with_id(0).generate_default();
+    mock_running_system
+        .assert_add_tx_success(&account0_invoke_nonce1)
+        .await
+
+    // FIXME: nonce=0 should always be deploy-account, but this currently goes into the queue.
+    // Figure out why this is currently allowed, fix it or remove this comment.
+    let account1_invoke_nonce0 = tx_generator.account_with_id(1).generate_default();
+    expected_tx_hashes.push(
+        mock_running_system
+            .assert_add_tx_success(&account1_invoke_nonce0)
+            .await,
+    );
+
+    let account0_invoke_nonce2 = tx_generator.account_with_id(0).generate_default();
+    mock_running_system
+        .assert_add_tx_success(&account0_invoke_nonce2)
+        .await;
 
     let mempool_txs = mock_running_system.get_txs(4).await;
-    assert_eq!(mempool_txs.len(), 3);
+    // Only the nonce-0 txs are returned, because we haven't merged queue-replenishment yet.
+    // This assert should be replaced with 4 once queue-replenishment is merged, also add a tx hole
+    // at that point, and ensure the assert doesn't change due to that.
+    assert_eq!(mempool_txs.len(), 2);
     let mut actual_tx_hashes: Vec<TransactionHash> =
         mempool_txs.iter().map(|tx| tx.tx_hash).collect();
     actual_tx_hashes.sort();
