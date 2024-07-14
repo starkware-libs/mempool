f8c7d6a015177126c1c1f083dfe2171d30d77a2a
Yair Bakalchuk <yair@starkware.co>
2024-07-14 10:33:08 +0300

    refactor

--- crates/gateway/src/rpc_state_reader_test.rs
+++ crates/gateway/src/rpc_state_reader_test.rs
@@ -1,3 +1,4 @@
+use rstest::rstest;
 use serde::Serialize;
 use serde_json::json;
 use starknet_api::block::{BlockNumber, GasPrice};
@@ -21,60 +22,68 @@ fn mock_rpc_interaction(
     params: impl Serialize,
     expected_response: &RpcResponse,
 ) -> mockito::Mock {
-    let request_body = mockito::Matcher::Json(json!({
+    let request_body = json!({
         "jsonrpc": json_rpc_version,
         "id": 0,
         "method": method,
         "params": json!(params),
-    }));
-    dbg!(serde_json::to_string(&expected_response).unwrap());
+    });
     server
         .mock("POST", "/")
         .match_header("Content-Type", "application/json")
-        .match_body(request_body)
+        .match_body(mockito::Matcher::Json(request_body))
         .with_status(201)
         .with_body(serde_json::to_string(expected_response).unwrap())
         .create()
 }
 
-// TODO(yair): Create case for each method.
+#[rstest]
+#[case::get_block_info(
+    "starknet_getBlockWithTxHashes",
+    GetBlockWithTxHashesParams { block_id: BlockId::Latest },
+    RpcResponse::Success(RpcSuccessResponse {
+        result: serde_json::to_value(BlockHeader {
+            block_number: BlockNumber(100),
+            // GasPrice must be non-zero.
+            l1_gas_price: ResourcePrice {
+                price_in_wei: GasPrice(1),
+                price_in_fri: GasPrice(1),
+            },
+            l1_data_gas_price: ResourcePrice {
+                price_in_wei: GasPrice(1),
+                price_in_fri: GasPrice(1),
+            },
+            ..Default::default()
+        })
+        .unwrap(),
+        ..Default::default()
+    }),
+    |client: &RpcStateReader| {
+        let block_info = client.get_block_info().unwrap();
+        // TODO(yair): Add partial_eq for BlockInfo and assert_eq the whole BlockInfo.
+        assert_eq!(block_info.block_number, BlockNumber(100));
+    }
+)]
 #[tokio::test]
-async fn test_get_block_info() {
+async fn test_get_block_info(
+    #[case] method: &str,
+    #[case] params: impl Serialize,
+    #[case] expected_response: RpcResponse,
+    #[case] client_call: fn(&RpcStateReader) -> (),
+) {
     let mut server = run_rpc_server().await;
     let config = RpcStateReaderConfig { url: server.url(), ..Default::default() };
 
     let mock = mock_rpc_interaction(
         &mut server,
         &config.json_rpc_version,
-        "starknet_getBlockWithTxHashes",
-        GetBlockWithTxHashesParams { block_id: BlockId::Latest },
-        &RpcResponse::Success(RpcSuccessResponse {
-            result: serde_json::to_value(BlockHeader {
-                block_number: BlockNumber(100),
-                // GasPrice must be non-zero.
-                l1_gas_price: ResourcePrice {
-                    price_in_wei: GasPrice(1),
-                    price_in_fri: GasPrice(1),
-                },
-                l1_data_gas_price: ResourcePrice {
-                    price_in_wei: GasPrice(1),
-                    price_in_fri: GasPrice(1),
-                },
-                ..Default::default()
-            })
-            .unwrap(),
-            ..Default::default()
-        }),
+        method,
+        params,
+        &expected_response,
     );
 
     let client = RpcStateReader::from_latest(&config);
-    tokio::task::spawn_blocking(move || {
-        let block_info = client.get_block_info().unwrap();
-        // TODO(yair): Add partial_eq for BlockInfo and assert_eq the whole BlockInfo.
-        assert_eq!(block_info.block_number, BlockNumber(100));
-    })
-    .await
-    .unwrap();
+    tokio::task::spawn_blocking(move || client_call(&client)).await.unwrap();
 
     mock.assert_async().await;
 }
