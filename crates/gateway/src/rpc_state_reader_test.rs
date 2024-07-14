654604ef0440fc92182be5e9db101c2a4c877eb6
Yair Bakalchuk <yair@starkware.co>
2024-07-14 13:27:49 +0300

    test(gateway): add more tests for RpcStateReader

--- crates/gateway/src/rpc_state_reader_test.rs
+++ crates/gateway/src/rpc_state_reader_test.rs
@@ -1,11 +1,18 @@
+use blockifier::execution::contract_class::ContractClass;
+use blockifier::state::state_api::StateReader;
+use cairo_lang_starknet_classes::casm_contract_class::CasmContractClass;
+use papyrus_rpc::CompiledContractClass;
 use rstest::rstest;
 use serde::Serialize;
 use serde_json::json;
 use starknet_api::block::{BlockNumber, GasPrice};
+use starknet_api::core::{ClassHash, ContractAddress, Nonce, PatriciaKey};
+use starknet_api::{class_hash, contract_address, felt, patricia_key};
 
 use crate::config::RpcStateReaderConfig;
 use crate::rpc_objects::{
-    BlockHeader, BlockId, GetBlockWithTxHashesParams, ResourcePrice, RpcResponse,
+    BlockHeader, BlockId, GetBlockWithTxHashesParams, GetClassHashAtParams,
+    GetCompiledContractClassParams, GetNonceParams, GetStorageAtParams, ResourcePrice, RpcResponse,
     RpcSuccessResponse,
 };
 use crate::rpc_state_reader::RpcStateReader;
@@ -64,8 +71,65 @@ fn mock_rpc_interaction(
         assert_eq!(block_info.block_number, BlockNumber(100));
     }
 )]
+#[case::get_storage_at(
+    "starknet_getStorageAt",
+    GetStorageAtParams { block_id: BlockId::Latest, contract_address: contract_address!("0x1"), key: starknet_api::state::StorageKey::from(0u32),},
+    RpcResponse::Success(RpcSuccessResponse {
+        result: serde_json::to_value(felt!("0x999")).unwrap(),
+        ..Default::default()
+    }),
+    |client: &RpcStateReader| {
+        let storage_value = client.get_storage_at(
+            contract_address!("0x1"),
+            starknet_api::state::StorageKey::from(0u32),
+        ).unwrap();
+        assert_eq!(storage_value, felt!("0x999"));
+    }
+)]
+#[case::get_nonce_at(
+    "starknet_getNonce",
+    GetNonceParams { block_id: BlockId::Latest, contract_address: contract_address!("0x1")},
+    RpcResponse::Success(RpcSuccessResponse {
+        result: serde_json::to_value(felt!("0x999")).unwrap(),
+        ..Default::default()
+    }),
+    |client: &RpcStateReader| {
+        let nonce = client.get_nonce_at(
+            contract_address!("0x1"),
+        ).unwrap();
+        assert_eq!(nonce, Nonce(felt!("0x999")));
+    }
+)]
+#[case::get_compiled_contract_class(
+    "starknet_getCompiledContractClass",
+    GetCompiledContractClassParams { block_id: BlockId::Latest, class_hash: class_hash!("0x1")},
+    RpcResponse::Success(RpcSuccessResponse {
+        result: serde_json::to_value(CompiledContractClass::V1(CasmContractClass::default())).unwrap(),
+        ..Default::default()
+    }),
+    |client: &RpcStateReader| {
+        let compiled_class = client.get_compiled_contract_class(
+            class_hash!("0x1"),
+        ).unwrap();
+        assert_eq!(compiled_class, ContractClass::V1(CasmContractClass::default().try_into().unwrap()));
+    }
+)]
+#[case::get_class_hash_at(
+    "starknet_getClassHashAt",
+    GetClassHashAtParams { block_id: BlockId::Latest, contract_address: contract_address!("0x1")},
+    RpcResponse::Success(RpcSuccessResponse {
+        result: serde_json::to_value(class_hash!("0x999")).unwrap(),
+        ..Default::default()
+    }),
+    |client: &RpcStateReader| {
+        let class_hash = client.get_class_hash_at(
+            contract_address!("0x1"),
+        ).unwrap();
+        assert_eq!(class_hash, class_hash!("0x999"));
+    }
+)]
 #[tokio::test]
-async fn test_get_block_info(
+async fn test_rpc_state_reader_happy_flow(
     #[case] method: &str,
     #[case] params: impl Serialize,
     #[case] expected_response: RpcResponse,
