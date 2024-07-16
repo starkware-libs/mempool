0aba599f9ea4735e18ab8fb1bad2b37e36f9910d
Ayelet Zilber <ayelet@starkware.co>
2024-07-16 15:02:17 +0300

    test(mempool): rename get_txs test to be more specific

--- crates/mempool/src/mempool_test.rs
+++ crates/mempool/src/mempool_test.rs
@@ -140,7 +140,7 @@ fn assert_eq_mempool_queue(mempool: &Mempool, expected_queue: &[ThinTransaction]
 #[case::test_get_exactly_all_eligible_txs(3)]
 #[case::test_get_more_than_all_eligible_txs(5)]
 #[case::test_get_less_than_all_eligible_txs(2)]
-fn test_get_txs(#[case] requested_txs: usize) {
+fn test_get_txs_fetching_order(#[case] requested_txs: usize) {
     let tx_tip_20_account_0 = add_tx_input!(tip: 20, tx_hash: 1).tx;
     let tx_tip_30_account_1 = add_tx_input!(tip: 30, tx_hash: 2, sender_address: "0x1").tx;
     let tx_tip_10_account_2 = add_tx_input!(tip: 10, tx_hash: 3, sender_address: "0x2").tx;
