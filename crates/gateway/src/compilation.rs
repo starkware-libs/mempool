fbf27481eef89e70bd343df90d93b2bb26fce7dc
Arni Hod <arnon@starkware.co>
2024-07-22 14:06:10 +0300

    chore: compiler will raise error on casm bytecode size limit

--- crates/gateway/src/compilation.rs
+++ crates/gateway/src/compilation.rs
@@ -70,18 +70,11 @@ impl GatewayCompiler {
     // the validation of the raw class size. The validation should be linked to the way the class is
     // saved in Papyrus etc.
     /// Validates that the Casm class is within size limit. Specifically, this function validates
-    /// the size of the bytecode and the serialized class.
+    /// the size of the serialized class.
     fn validate_casm_class_size(
         &self,
         casm_contract_class: &CasmContractClass,
     ) -> Result<(), GatewayError> {
-        let bytecode_size = casm_contract_class.bytecode.len();
-        if bytecode_size > self.config.max_casm_bytecode_size {
-            return Err(GatewayError::CasmBytecodeSizeTooLarge {
-                bytecode_size,
-                max_bytecode_size: self.config.max_casm_bytecode_size,
-            });
-        }
         let contract_class_object_size = serde_json::to_string(&casm_contract_class)
             .expect("Unexpected error serializing Casm contract class.")
             .len();
