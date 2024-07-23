08a9987d2307cab27f01788563d81c0fcacc14cd
Uriel Korach <uriel@starkware.co>
2024-07-23 14:08:02 +0300

    refactor: sort and rename component clients into dedicated dirs

---
+++ crates/mempool_infra/src/component_client/mod.rs
@@ -0,0 +1,24 @@
+use bincode::ErrorKind;
+use hyper::{Error as HyperError, StatusCode};
+use thiserror::Error;
+
+use crate::component_definitions::ServerError;
+
+pub mod local_component_client;
+pub mod remote_component_client;
+
+#[derive(Debug, Error)]
+pub enum ClientError {
+    #[error("Communication error: {0}")]
+    CommunicationFailure(HyperError),
+    #[error("Could not deserialize server response: {0}")]
+    ResponseDeserializationFailure(Box<ErrorKind>),
+    #[error("Could not parse the response: {0}")]
+    ResponseParsingFailure(HyperError),
+    #[error("Got status code: {0}, with server error: {1}")]
+    ResponseError(StatusCode, ServerError),
+    #[error("Got an unexpected response type.")]
+    UnexpectedResponse,
+}
+
+pub type ClientResult<T> = Result<T, ClientError>;
